d46c6d6332184f93d3833c239a0f5d192d492820
Jason Bowman <jason.bowman@pm.me>
2025-01-15 21:26:28 -0800

    stash bond config

--- systems/x86_64-linux/surge/default.nix
+++ systems/x86_64-linux/surge/default.nix
@@ -16,7 +16,55 @@
     btrfs_profile = "single";
   };
 
-  hardware.networking.enable = true;
+  hardware.networking.enable = false;
+  #networking.eno1.ipv4.addresses = [ "10.10.10.6" ];
+  #networking.enp2s0.ipv4.addresses = [ "10.10.10.5" ];
+
+  # Networking
+  systemd.network = {
+    enable = true;
+    netdevs = {
+      "10-bond0" = {
+        netdevConfig = {
+          Kind = "bond";
+          Name = "bond0";
+        };
+        bondConfig = {
+          # Mode = "balance-rr";
+          # Mode = "balance-alb";
+          Mode = "balance-xor";
+          TransmitHashPolicy = "layer3+4";
+        };
+      };
+    };
+    # Configure Bonds to utilize both 2.5Gbps ports
+    networks = {
+      "30-eno1" = {
+        matchConfig.Name = "eno1";
+        networkConfig.Bond = "bond0";
+      };
+
+      "30-enp2s0" = {
+        matchConfig.Name = "enp2s0";
+        networkConfig.Bond = "bond0";
+      };
+
+      "40-bond0" = {
+        matchConfig.Name = "bond0";
+        linkConfig = {
+          RequiredForOnline = "routable";
+        };
+        address = [ "10.10.10.5/16" ];
+        gateway = [ "10.10.0.1" ];
+        routes = [
+          { Gateway = "10.10.0.1"; }
+        ];
+      };
+    };
+  };
+
+  networking.nameservers = [ "1.1.1.1" "8.8.8.8" ];
+  networking.firewall.enable = false;
 
   services.ssh.enable = true;
   programs.dconf.enable = true;
