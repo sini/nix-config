3418cb96873826942dcafadbd7ef7caf1ace93af
Jason Bowman <jason@json64.dev>
2025-02-05 16:13:17 -0800

    cleanup

---
+++ parts/pkgs.nix
@@ -0,0 +1,58 @@
+{ inputs, ... }:
+let
+  # Utility function to construct a package set based on the given system
+  # along with the shared `nixpkgs` configuration defined in this repo.
+  mkPkgsFor =
+    system: pkgset:
+    import pkgset {
+      inherit system;
+      config = {
+        allowUnfree = true;
+      };
+    };
+in
+{
+  imports = [
+    (
+      {
+        lib,
+        flake-parts-lib,
+        ...
+      }:
+      flake-parts-lib.mkTransposedPerSystemModule {
+        name = "pkgs";
+        file = ./pkgs.nix;
+        option = lib.mkOption {
+          type = lib.types.unspecified;
+        };
+      }
+    )
+  ];
+
+  perSystem =
+    { system, ... }:
+    {
+      _module.args = rec {
+        # Provide un-imported package set paths for reference in other modules.
+        pkgsets = {
+          unstable = inputs.nixpkgs-unstable;
+          # Select the Nix package path based on the system being managed.
+          nixpkgs =
+            if (builtins.match ".*darwin" system != null) then inputs.nixpkgs-darwin else inputs.nixpkgs;
+        };
+
+        # Import the default Nix package set with the common config.
+        pkgs = mkPkgsFor system pkgsets.nixpkgs;
+
+        # Import the unstable Nix package set with the common config.
+        unstable = mkPkgsFor system pkgsets.nixpkgs-unstable;
+
+        # Select the home-manager input based on the system being managed.
+        homeManager =
+          if (builtins.match ".*darwin" system != null) then
+            inputs.home-manager-darwin
+          else
+            inputs.home-manager;
+      };
+    };
+}
--- parts/systems.nix
+++ parts/systems.nix
@@ -1,93 +1,146 @@
-{ inputs, ... }:
 {
-  flake =
+  self,
+  inputs,
+  lib,
+  withSystem,
+  ...
+}:
+let
+  inherit (lib)
+    mapAttrs
+    ;
+  namespace = "custom";
+  extendedLib = inputs.nixpkgs.lib.extend (_self: _super: import ../lib _self namespace);
+
+  shared_modules =
+    extendedLib.${namespace}.listModuleDefaultsRec (
+      extendedLib.${namespace}.relativeToRoot "modules/shared"
+    )
+    ++ [
+      inputs.agenix.nixosModules.default
+      inputs.agenix-rekey.nixosModules.default
+    ];
+
+  nixos_modules =
+    shared_modules
+    ++ extendedLib.${namespace}.listModuleDefaultsRec (
+      extendedLib.${namespace}.relativeToRoot "modules/nixos"
+    )
+    ++ [
+      inputs.nixos-facter-modules.nixosModules.facter
+      inputs.disko.nixosModules.disko
+    ];
+
+  darwin_modules =
+    shared_modules
+    ++ extendedLib.${namespace}.listModuleDefaultsRec (
+      extendedLib.${namespace}.relativeToRoot "modules/darwin"
+    );
+
+  mkNixOSConfigWith =
     {
-      config,
-      lib,
-      ...
+      hostname,
+      system,
+      path,
+      extraModules ? [ ],
     }:
-    let
-      inherit (lib)
-        mapAttrs
-        ;
-      namespace = "custom";
-      extendedLib = inputs.nixpkgs.lib.extend (_self: _super: import ../lib _self namespace);
+    withSystem system (
+      {
+        pkgsets,
+        pkgs,
+        unstable,
+        homeManager,
+        ...
+      }:
+      pkgsets.nixpkgs.lib.nixosSystem rec {
+        inherit system;
+        specialArgs = {
+          inherit
+            inputs
+            pkgsets
+            pkgs
+            unstable
+            ;
+          lib = extendedLib;
+          namespace = "custom";
+        };
+        modules =
+          extraModules
+          ++ nixos_modules
+          ++ [
+            homeManager.darwinModules.home-manager
+            {
+              networking.hostName = hostname;
+            }
+            path
+          ];
+      }
+    );
 
-      shared_modules =
-        extendedLib.${namespace}.listModuleDefaultsRec (
-          extendedLib.${namespace}.relativeToRoot "modules/shared"
-        )
-        ++ [
-          inputs.agenix.nixosModules.default
-          inputs.agenix-rekey.nixosModules.default
-        ];
-
-      nixos_modules =
-        shared_modules
-        ++ extendedLib.${namespace}.listModuleDefaultsRec (
-          extendedLib.${namespace}.relativeToRoot "modules/nixos"
-        )
-        ++ [
-          inputs.nixos-facter-modules.nixosModules.facter
-          inputs.disko.nixosModules.disko
-        ];
-
-      darwin_modules =
-        shared_modules
-        ++ extendedLib.${namespace}.listModuleDefaultsRec (
-          extendedLib.${namespace}.relativeToRoot "modules/darwin"
-        );
-
-      inherit (extendedLib.${namespace}) linuxHosts darwinHosts;
-    in
+  mkDarwinConfigWith =
     {
-      nixosConfigurations = lib.attrsets.mergeAttrsList (
-        builtins.map (_elem: {
-          "${_elem.hostname}" = inputs.nixpkgs.lib.nixosSystem {
-            specialArgs = {
-              inherit
-                inputs
-                ;
-              lib = extendedLib;
-              namespace = "custom";
-            };
-            modules = [
-              {
-                nixpkgs.config.allowUnfree = true;
-                networking.hostName = _elem.hostname;
-              }
-              _elem.path
-            ] ++ nixos_modules;
+      hostname,
+      system,
+      path,
+      extraModules ? [ ],
+    }:
+    withSystem system (
+      {
+        pkgsets,
+        pkgs,
+        unstable,
+        homeManager,
+        ...
+      }:
+      inputs.nix-darwin.lib.darwinSystem rec {
+        inherit system;
+        specialArgs = {
+          inherit
+            pkgsets
+            pkgs
+            unstable
+            ;
+          lib = extendedLib;
+          namespace = "custom";
+          inputs = inputs // {
+            inherit (pkgsets) nixpkgs;
           };
-        }) linuxHosts
-      );
+        };
+        modules =
+          extraModules
+          ++ darwin_modules
+          ++ [
+            pkgsets.nixpkgs.nixosModules.notDetected
+            homeManager.nixosModules.home-manager
+            {
+              networking.hostName = hostname;
+            }
+            path
+          ];
+      }
+    );
 
-      darwinConfigurations = lib.attrsets.mergeAttrsList (
-        builtins.map (_elem: {
-          "${_elem.hostname}" = inputs.nixpkgs.lib.nixosSystem {
-            specialArgs = {
-              inherit
-                inputs
-                ;
-              lib = extendedLib;
-              namespace = "custom";
-            };
-            modules = [
-              {
-                nixpkgs.config.allowUnfree = true;
-                networking.hostName = _elem.hostname;
-              }
-              _elem.path
-            ] ++ darwin_modules;
-          };
-        }) darwinHosts
-      );
+  inherit (extendedLib.${namespace}) linuxHosts darwinHosts;
+in
+{
+  flake = {
+    nixosConfigurations = lib.attrsets.mergeAttrsList (
+      builtins.map (_elem: {
+        "${_elem.hostname}" = mkNixOSConfigWith { inherit (_elem) hostname system path; };
+      }) linuxHosts
+    );
 
-      # All nixosSystem instanciations are collected here, so that we can refer
-      # to any system via nodes.<name>
-      nodes = config.nixosConfigurations // config.darwinConfigurations;
+    darwinConfigurations = lib.attrsets.mergeAttrsList (
+      builtins.map (_elem: {
+        "${_elem.hostname}" = mkDarwinConfigWith { inherit (_elem) hostname system path; };
+      }) darwinHosts
+    );
 
-      # Add a shorthand to easily target toplevel derivations
-      "@" = mapAttrs (_: v: v.config.system.build.toplevel) config.nodes;
-    };
+    # All nixosSystem instanciations are collected here, so that we can refer
+    # to any system via nodes.<name>
+    nodes = self.outputs.nixosConfigurations // self.outputs.darwinConfigurations;
+
+    # Add a shorthand to easily target toplevel derivations
+    "@" = mapAttrs (_: v: v.config.system.build.toplevel) self.outputs.nodes;
+  };
 }
