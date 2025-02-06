8d257994ba45ee5d869a9619636e5135e2bbe584
Jason Bowman <jason.bowman@pm.me>
2025-02-06 13:39:49 -0800

    add new host: uplink

---
+++ systems/x86_64-linux/uplink/default.nix
@@ -0,0 +1,49 @@
+{ pkgs, ... }:
+{
+  facter.reportPath = ./facter.json;
+
+  node.deployment = {
+    tags = [ "server" ];
+    targetHost = "10.10.10.1";
+  };
+
+  # topology.self = {
+  #   hardware.info = "burst";
+  #   services.k8s.name = "k8s";
+  # };
+
+  hardware.disk.single = {
+    enable = true;
+    device_id = "nvme-Samsung_SSD_990_PRO_2TB_S73WNJ0W310395L";
+  };
+
+  hardware.networking.enable = true;
+
+  services.ssh.enable = true;
+  programs.dconf.enable = true;
+
+  system = {
+    nix.enable = true;
+    security.doas.enable = true;
+  };
+
+  time.timeZone = "America/Los_Angeles";
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  environment.systemPackages = with pkgs; [
+    # Any particular packages only for this host
+    wget
+    vim
+    git
+    doas
+    doas-sudo-shim
+  ];
+
+  services = {
+    rpcbind.enable = true; # needed for NFS
+  };
+
+  # ======================== DO NOT CHANGE THIS ========================
+  system.stateVersion = "24.11";
+  # ======================== DO NOT CHANGE THIS ========================
+}
