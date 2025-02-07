26ccfedb410f1339cb8c25035f69b0c25a955a03
Jason Bowman <jason.bowman@pm.me>
2025-02-07 14:05:32 -0800

    fix formatting

--- systems/aarch64-darwin/patch/default.nix
+++ systems/aarch64-darwin/patch/default.nix
@@ -180,27 +180,27 @@
     enable = true;
     useBabelfish = true;
     loginShellInit =
-    let
-      # We should probably use `config.environment.profiles`, as described in
-      # https://github.com/LnL7/nix-darwin/issues/122#issuecomment-1659465635
-      # but this takes into account the new XDG paths used when the nix
-      # configuration has `use-xdg-base-directories` enabled. See:
-      # https://github.com/LnL7/nix-darwin/issues/947 for more information.
-      profiles = [
-        "/etc/profiles/per-user/$USER" # Home manager packages
-        "$HOME/.nix-profile"
-        "(set -q XDG_STATE_HOME; and echo $XDG_STATE_HOME; or echo $HOME/.local/state)/nix/profile"
-        "/run/current-system/sw"
-        "/nix/var/nix/profiles/default"
-      ];
+      let
+        # We should probably use `config.environment.profiles`, as described in
+        # https://github.com/LnL7/nix-darwin/issues/122#issuecomment-1659465635
+        # but this takes into account the new XDG paths used when the nix
+        # configuration has `use-xdg-base-directories` enabled. See:
+        # https://github.com/LnL7/nix-darwin/issues/947 for more information.
+        profiles = [
+          "/etc/profiles/per-user/$USER" # Home manager packages
+          "$HOME/.nix-profile"
+          "(set -q XDG_STATE_HOME; and echo $XDG_STATE_HOME; or echo $HOME/.local/state)/nix/profile"
+          "/run/current-system/sw"
+          "/nix/var/nix/profiles/default"
+        ];
 
-      makeBinSearchPath = lib.concatMapStringsSep " " (path: "${path}/bin");
-    in
-    ''
-      # Fix path that was re-ordered by Apple's path_helper
-      fish_add_path --move --prepend --path ${makeBinSearchPath profiles}
-      set fish_user_paths $fish_user_paths
-    '';
+        makeBinSearchPath = lib.concatMapStringsSep " " (path: "${path}/bin");
+      in
+      ''
+        # Fix path that was re-ordered by Apple's path_helper
+        fish_add_path --move --prepend --path ${makeBinSearchPath profiles}
+        set fish_user_paths $fish_user_paths
+      '';
   };
   # ======================== DO NOT CHANGE THIS ========================
   system.stateVersion = 5;
