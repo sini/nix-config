046a49adee08bbf3a0561496596eac40cd92a624
Jason Bowman <jason.bowman@pm.me>
2025-02-19 16:38:43 -0800

    stash user changes

--- modules/system/common/options/users/default.nix
+++ modules/system/common/options/users/default.nix
@@ -5,9 +5,6 @@
 }:
 let
   inherit (lib)
-    concatLists
-    flip
-    mapAttrsToList
     mkDefault
     mkIf
     mkOption
@@ -76,27 +73,4 @@ in
     };
   };
 
-  config = {
-    assertions =
-      concatLists (
-        flip mapAttrsToList config.users.users (
-          name: user: [
-            {
-              assertion = user.uid != null;
-              message = "non-deterministic uid detected for '${name}', please assign one via `users.deterministicIds`";
-            }
-            {
-              assertion = !user.autoSubUidGidRange;
-              message = "non-deterministic subUids/subGids detected for: ${name}";
-            }
-          ]
-        )
-      )
-      ++ flip mapAttrsToList config.users.groups (
-        name: group: {
-          assertion = group.gid != null;
-          message = "non-deterministic gid detected for '${name}', please assign one via `users.deterministicIds`";
-        }
-      );
-  };
 }
