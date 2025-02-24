e3acbd9df0eeee343bf006cd13b4144b8697ff7a
Jason Bowman <jason@json64.dev>
2025-02-24 09:33:14 -0800

    Add nixd to devshell

--- modules/flake/devshell.nix
+++ modules/flake/devshell.nix
@@ -20,6 +20,7 @@
             pkgs.nixos-rebuild # Ensure nixos-rebuild is available for darwin systems
             pkgs.nix-output-monitor
             pkgs.nil
+            pkgs.nixd
           ]
           ++ lib.optionals pkgs.buildPlatform.isDarwin [
             pkgs.coreutils-full # Include GNU coreutils for darwin systems
