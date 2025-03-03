f5c02bdca32b5c7565a610ab83ba44c89f34ddcc
Jason Bowman <jason@json64.dev>
2025-03-03 12:02:38 -0800

    stash networking changes

--- modules/system/nixos/hardware/networking/default.nix
+++ modules/system/nixos/hardware/networking/default.nix
@@ -13,10 +13,25 @@ in
 {
   options.hardware.networking = with types; {
     enable = mkBoolOpt false "Enable networkmanager";
+    interface = mkOption {
+      type = types.str;
+      default = "enp1s0";
+      description = "The interface to configure";
+    };
   };
 
   config = mkIf cfg.enable {
-    networking.networkmanager.enable = false;
-    systemd.network.wait-online.enable = false;
+    systemd.network = {
+      enable = true;
+      wait-online.enable = false;
+      networks."10-lan" = {
+        matchConfig.Name = cfg.interface;
+        networkConfig.DHCP = "yes";
+      };
+    };
+
+    #TODO: Remove this once we have a better way to expose ports...
+    networking.firewall.enable = false;
+
   };
 }
