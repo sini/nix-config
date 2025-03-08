be1a8064580c7c305b3d806a305145b30fbfeac1
Jason Bowman <jason@json64.dev>
2025-03-07 16:55:30 -0800

    add media user, update podman ettings

--- modules/system/nixos/services/podman/default.nix
+++ modules/system/nixos/services/podman/default.nix
@@ -16,17 +16,16 @@ in
   };
 
   config = mkIf cfg.enable {
-    # NixOS 22.05 moved NixOS Containers to a new state directory and the old
-    # directory is taken over by OCI Containers (eg. podman). For systems with
-    # system.stateVersion < 22.05, it is not possible to have both enabled.
-    # This option disables NixOS Containers, leaving OCI Containers available.
-    boot.enableContainers = false;
-
     environment.systemPackages = with pkgs; [
+      dive # look into docker image layers
       podman-compose
+      podman-tui # Terminal mgmt UI for Podman
+      passt # For Pasta rootless networking
     ];
 
     virtualisation = {
+      containers.enable = true;
+      oci-containers.backend = "podman";
       podman = {
         inherit (cfg) enable;
 
@@ -42,9 +41,20 @@ in
         dockerSocket.enable = true;
       };
 
-      oci-containers = {
-        backend = "podman";
+      containers.storage.settings = {
+        storage = {
+          driver = "btrfs";
+          runroot = "/run/containers/storage";
+          graphroot = "/var/lib/containers/storage";
+          options.overlay.mountopt = "nodev,metacopy=on";
+        }; # storage
       };
     };
+
+    # Add 'newuidmap' and 'sh' to the PATH for users' Systemd units.
+    # Required for Rootless podman.
+    systemd.user.extraConfig = ''
+      DefaultEnvironment="PATH=/run/current-system/sw/bin:/run/wrappers/bin:${lib.makeBinPath [ pkgs.bash ]}"
+    '';
   };
 }
---
+++ modules/system/nixos/user/mainUser.nix
@@ -0,0 +1,46 @@
+{
+  config,
+  lib,
+  namespace,
+  ...
+}:
+with lib;
+with lib.${namespace};
+let
+  cfg = config.node.mainUser;
+in
+{
+  config = {
+    users = {
+      mutableUsers = false;
+
+      groups.${cfg}.gid = config.users.users.${cfg}.uid;
+      users.${cfg} = {
+        isNormalUser = true;
+        # inherit (cfg) name initialPassword;
+        initialHashedPassword = "$y$j9T$RpfkDk8AusZr9NS09tJ9e.$kbc4SL9Cu45o1YYPlyV1jiVTZZ/126ue5Nff2Rfgpw8";
+
+        home = "/home/${cfg}";
+        autoSubUidGidRange = false;
+
+        group = cfg;
+
+        openssh.authorizedKeys.keys = [
+          "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC2KomSUc6hK7QyOCb1AAG00S7ZqVeXqGKvS0po5HishO6YFgr9cPvST6rdxAYreO6b20LQ8e4Rns3yrGNekWww8Yl32dFdmv0sC1VPZrfJPFKg0qC+imjk3vGDohYII9/3cyDBBb2WuZzupCGSTi+g14AA6/csJXYwN0bQfh/XmLp1OrbrFzmCZEwAWFni95DNMo5WxLeqdUXJxM6is77AzLYbRX7TQqBvdaTyyGjzh6uVi6CkDVJSnhMp3kPRhzqudXyW1RN680U+tgsyDhX+S5AHxgqHZ1OWLkKl+N87ov77rawGXVUEQO1d2ZnOcIwnTQak6rgyiLtPKY81if7mQm53LB0sEsM7Czm9sv1J0RbnR7HwjoygIApDeD29xfTvM4WlYpIn3pk1auS/ZTLQVqg8tx/WhNko5n+DsWCcSIPZ/chu3vs3dvegbYn9QTbEMfHxMp5iLbb3EOmNG08z9M+MQ2gIzbsDPE5KgsEfW84omc9iWy4JvEfvpPyOEKiRf7Ou8bawPDP6tvJv8P7fwEyxfRmhya8hM+ThbUEmPYydwUXJHZ2BkIXk+/1LsTg1lmfADqYb0i2I++1T3C7NbSvYsQ0BobQrIiulkVWzvb/1KuuRcGr4bRxxumJNzmmLWUJLUnWV/ya2h4FAoM/uRPyICGfGeejyycXN1q5mQ== cardno:31_057_490"
+        ];
+
+        extraGroups = [
+          "wheel"
+          "audio"
+          "sound"
+          "video"
+          "networkmanager"
+          "input"
+          "tty"
+          "podman"
+          "media"
+        ];
+      };
+    };
+  };
+}
