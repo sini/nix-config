de5e54a57b8eda5d2c94ec9a95258e4b9f9e9e7f
Jason Bowman <jason@json64.dev>
2025-03-07 20:25:56 -0800

    move initrd config

--- modules/system/nixos/services/ssh/default.nix
+++ modules/system/nixos/services/ssh/default.nix
@@ -15,36 +15,60 @@ in
     enable = mkBoolOpt false "Enable ssh";
   };
 
-  config = mkIf cfg.enable {
-    services.openssh = {
-      enable = true;
-      ports = [ 22 ];
+  config = mkMerge [
+    mkIf
+    cfg.enable
+    {
+      services.openssh = {
+        enable = true;
+        ports = [ 22 ];
 
-      openFirewall = true;
+        openFirewall = true;
 
-      settings.PermitRootLogin = "prohibit-password";
+        settings.PermitRootLogin = "prohibit-password";
 
-      settings = {
-        PasswordAuthentication = false;
-        KbdInteractiveAuthentication = false;
+        settings = {
+          PasswordAuthentication = false;
+          KbdInteractiveAuthentication = false;
+        };
+
+        extraConfig = ''
+          AllowTcpForwarding yes
+          X11Forwarding yes
+          AllowAgentForwarding yes
+          AllowStreamLocalForwarding yes
+          AuthenticationMethods publickey
+        '';
       };
 
-      extraConfig = ''
-        AllowTcpForwarding yes
-        X11Forwarding yes
-        AllowAgentForwarding yes
-        AllowStreamLocalForwarding yes
-        AuthenticationMethods publickey
-      '';
-    };
+      # Let all users with the wheel group have their keys in the authorized_keys for root
+      users.users.root.openssh.authorizedKeys.keys =
+        with lib;
+        concatLists (
+          mapAttrsToList (
+            _name: user: if elem "wheel" user.extraGroups then user.openssh.authorizedKeys.keys else [ ]
+          ) config.users.users
+        );
+    }
+    {
+      # Make sure that there is always a valid initrd hostkey available that can be installed into
+      # the initrd. When bootstrapping a system (or re-installing), agenix cannot succeed in decrypting
+      # whatever is given, since the correct hostkey doesn't even exist yet. We still require
+      # a valid hostkey to be available so that the initrd can be generated successfully.
+      # The correct initrd host-key will be installed with the next update after the host is booted
+      # for the first time, and the secrets were rekeyed for the the new host identity.
 
-    # Let all users with the wheel group have their keys in the authorized_keys for root
-    users.users.root.openssh.authorizedKeys.keys =
-      with lib;
-      concatLists (
-        mapAttrsToList (
-          _name: user: if elem "wheel" user.extraGroups then user.openssh.authorizedKeys.keys else [ ]
-        ) config.users.users
-      );
-  };
+      system.activationScripts.agenixEnsureInitrdHostkey = {
+        text = ''
+          [[ -e ${config.age.secrets.initrd_host_ed25519_key.path} ]] \
+            || ${pkgs.openssh}/bin/ssh-keygen -t ed25519 -N "" -f ${config.age.secrets.initrd_host_ed25519_key.path}
+        '';
+        deps = [
+          "agenixInstall"
+          "users"
+        ];
+      };
+      system.activationScripts.agenixChown.deps = [ "agenixEnsureInitrdHostkey" ];
+    }
+  ];
 }
