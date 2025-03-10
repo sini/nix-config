528b2c46ffc3be4fe46f32042682f463828ad3c5
Jason Bowman <jason@json64.dev>
2025-03-10 13:08:58 -0700

    stash

--- modules/system/nixos/services/media/data-share/default.nix
+++ modules/system/nixos/services/media/data-share/default.nix
@@ -8,10 +8,10 @@
 with lib;
 with lib.${namespace};
 let
-  cfg = config.services.data-share;
+  cfg = config.services.media.data-share;
 in
 {
-  options.services.data-share = with types; {
+  options.services.media.data-share = with types; {
     enable = mkBoolOpt false "Enable NFS data share mount";
   };
 
@@ -21,18 +21,72 @@ in
       "nfs4"
     ];
 
+    services.rpcbind.enable = true; # needed for NFS
+
     fileSystems."/mnt/data" = {
       device = "10.10.10.10:/volume2/data";
-      fsType = "nfs";
+      fsType = "nfs4";
       options = [
-        "x-systemd.automount"
-        "noauto"
-        "x-systemd.idle-timeout=300"
-        "noatime"
+        "_netdev"
         "nfsvers=4.1"
-        "remoteaddrs=10.10.10.10-10.10.10.12"
+        "noauto"
+        "noatime"
+        "x-systemd.automount"
+        "x-systemd.idle-timeout=600"
       ];
     };
-    #fileSystems."/mnt/NAS" = { device = "srv-prod-nas.home.address:/mnt/Main Storage/USER_NAME/USER_NAME"; fsType = "nfs"; };
+
+    # systemd.mounts =
+    #   let
+    #     commonMountOptions = {
+    #       type = "nfs";
+    #       mountConfig = {
+    #         Options = "noatime,nfsvers=4.1,remoteaddrs=10.10.10.10-10.10.10.12";
+    #       };
+    #     };
+    #   in
+
+    #   [
+    #     (
+    #       commonMountOptions
+    #       // {
+    #         what = "10.10.10.10:/volume2/data";
+    #         where = "/mnt/data";
+    #       }
+    #     )
+
+    #     (
+    #       commonMountOptions
+    #       // {
+    #         what = "10.10.10.10:/volume1/NVME";
+    #         where = "/mnt/NVME";
+    #       }
+    #     )
+
+    #     (
+    #       commonMountOptions
+    #       // {
+    #         what = "10.10.10.10:/volume1/docker";
+    #         where = "/mnt/docker";
+    #       }
+    #     )
+    #   ];
+
+    # systemd.automounts =
+    #   let
+    #     commonAutoMountOptions = {
+    #       wantedBy = [ "multi-user.target" ];
+    #       automountConfig = {
+    #         TimeoutIdleSec = "600";
+    #       };
+    #     };
+    #   in
+
+    #   [
+    #     (commonAutoMountOptions // { where = "/mnt/data"; })
+    #     (commonAutoMountOptions // { where = "/mnt/NVME"; })
+    #     (commonAutoMountOptions // { where = "/mnt/docker"; })
+    #   ];
+
   };
 }
