dfe3c4c5e1a27cd8566ee47144f0c20e1ef852eb
Jason Bowman <jason@json64.dev>
2025-03-12 12:48:00 -0700

    add intel arc support

---
+++ modules/system/nixos/hardware/gpu/intel-arc/default.nix
@@ -0,0 +1,63 @@
+# Based on https://github.com/akirak/homelab/blob/master/profiles/intel-arc/default.nix
+# Which was based on https://github.com/VTimofeenko/monorepo-machine-config/blob/4c1f85c700c45a5d3a8a38956194d2c97753b8ba/nixosConfigurations/neon/configuration/hw-acceleration.nix#L24
+{
+  config,
+  pkgs,
+  lib,
+  namespace,
+  ...
+}:
+with lib;
+with lib.${namespace};
+let
+  cfg = config.hardware.gpu.intel-arc;
+in
+{
+  options.hardware.gpu.intel-arc = with types; {
+    enable = mkBoolOpt false "Enable Intel ARC support";
+    device_id = mkOption {
+      type = types.str;
+      default = "22182";
+      description = "The PCI ID of the Intel ARC device";
+    };
+  };
+
+  config = mkIf cfg.enable {
+
+    # imports = [
+    #   (inputs.nixos-hardware.outPath + "/common/gpu/intel")
+    # ];
+
+    hardware.graphics = {
+      enable = true;
+      extraPackages = with pkgs; [
+        intel-media-driver
+        intel-ocl
+        # nixos-unstable
+        vpl-gpu-rt
+        intel-media-sdk
+        intel-compute-runtime
+        intel-vaapi-driver
+      ];
+    };
+
+    boot.kernelParams = [
+      # Check the ID by running `lspci -k | grep -EA3 'VGA|3D|Display'`
+      "i915.force_probe=${cfg.device_id}"
+      "i915.enable_guc=3"
+    ];
+
+    environment.sessionVariables = {
+      VDPAU_DRIVER = "va_gl";
+      LIBVA_DRIVER_NAME = "iHD";
+    };
+
+    boot.kernelPackages = pkgs.linuxPackages_latest;
+
+    environment.systemPackages = with pkgs; [
+      pciutils
+      libva-utils
+      intel-gpu-tools
+    ];
+  };
+}
