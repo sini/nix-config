28fe3bc821f520acdcdccbccbb9d79f5a390e9b8
Jason Bowman <jason@json64.dev>
2025-04-23 14:19:48 -0700

    fix network booting

--- modules/system/nixos/hardware/boot/initrd/default.nix
+++ modules/system/nixos/hardware/boot/initrd/default.nix
@@ -1,31 +1,81 @@
-_: {
-  boot = {
-    loader = {
-      systemd-boot = {
-        enable = true;
-        configurationLimit = 10;
-        consoleMode = "0"; # increase font size
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}:
+{
+  config = {
+    boot = {
+      initrd = {
+        availableKernelModules = [
+          "r8169"
+          "mlx4_core"
+          "mlx4_en"
+        ];
+
+        systemd = {
+          enable = true;
+
+          initrdBin = with pkgs; [
+            cryptsetup
+          ];
+
+          network = {
+            enable = true;
+            inherit (config.systemd.network) networks;
+          };
+
+          targets.network-pre = {
+            requires = [ "sys-subsystem-net-devices-${config.hardware.networking.interface}.device" ];
+            after = [ "sys-subsystem-net-devices-${config.hardware.networking.interface}.device" ];
+          };
+
+          users.root.shell = "/bin/systemd-tty-ask-password-agent";
+        };
+
+        network = {
+          enable = true;
+          ssh = {
+            enable = true;
+            port = 22;
+            authorizedKeys =
+              with lib;
+              concatLists (
+                mapAttrsToList (
+                  _name: user: if elem "wheel" user.extraGroups then user.openssh.authorizedKeys.keys else [ ]
+                ) config.users.users
+              );
+            hostKeys = [
+              config.age.secrets.initrd_host_ed25519_key.path
+            ];
+          };
+        };
+
       };
 
-      efi.canTouchEfiVariables = true;
+      loader = {
+        systemd-boot = {
+          enable = true;
+          configurationLimit = 10;
+          consoleMode = "0"; # increase font size
+        };
+
+        efi.canTouchEfiVariables = true;
+      };
+
+      tmp.cleanOnBoot = true;
+
+      kernelParams = [
+        # For AMD Zen 4 this is no longer needed: https://www.phoronix.com/news/AMD-Zen-4-Mitigations-Off
+        "mitigations=off"
+        #"quiet"
+        #"udev.log_level=3"
+      ];
+
+      #plymouth.enable = true; # Display loading screen
+
+      #consoleLogLevel = 0;
     };
-
-    initrd = {
-      systemd.enable = true;
-      verbose = false;
-    };
-
-    tmp.cleanOnBoot = true;
-
-    kernelParams = [
-      # For AMD Zen 4 this is no longer needed: https://www.phoronix.com/news/AMD-Zen-4-Mitigations-Off
-      "mitigations=off"
-      "quiet"
-      "udev.log_level=3"
-    ];
-
-    plymouth.enable = true; # Display loading screen
-
-    consoleLogLevel = 0;
   };
 }
