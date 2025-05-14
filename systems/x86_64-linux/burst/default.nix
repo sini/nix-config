f628dda6907a876eeb4cd5c9ca3236f9594ce97b
Jason Bowman <jason@json64.dev>
2025-05-14 09:51:26 -0700

    update drive layouts

--- systems/x86_64-linux/burst/default.nix
+++ systems/x86_64-linux/burst/default.nix
@@ -17,14 +17,20 @@
 
   boot.kernelPackages = unstable.linuxPackages_latest;
 
-  # topology.self = {
-  #   hardware.info = "burst";
-  #   services.k8s.name = "k8s";
-  # };
+  hardware = {
+    disk.longhorn = {
+      enable = true;
+      os_drive = {
+        device_id = "nvme-KINGSTON_OM8PGP41024Q-A0_50026B738300CCCC";
+        swap_size = 8192;
+      };
+      longhorn_drive = {
+        device_id = "nvme-Force_MP600_1925823000012856500E";
+      };
+    };
 
-  hardware.disk.single.enable = true;
-
-  hardware.networking.enable = true;
+    networking.enable = true;
+  };
 
   services.ssh.enable = true;
   programs.dconf.enable = true;
