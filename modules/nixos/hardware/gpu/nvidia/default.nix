35c38970e2c0d9414b8cf67fe79fd7439f061193
Jason Bowman <jason@json64.dev>
2025-06-16 10:46:31 -0700

    update credit

--- modules/nixos/hardware/gpu/nvidia/default.nix
+++ modules/nixos/hardware/gpu/nvidia/default.nix
@@ -1,5 +1,4 @@
-# Based on https://github.com/akirak/homelab/blob/master/profiles/intel-arc/default.nix
-# Which was based on https://github.com/VTimofeenko/monorepo-machine-config/blob/4c1f85c700c45a5d3a8a38956194d2c97753b8ba/nixosConfigurations/neon/configuration/hw-acceleration.nix#L24
+# Based on https://github.com/TLATER/dotfiles/blob/master/nixos-modules/nvidia/prime.nix
 {
   config,
   lib,
