a516714c072a1fe4fd79615941eabc6987032876
Jason Bowman <jason@json64.dev>
2025-06-16 11:05:53 -0700

    pin nvidia driver version

--- modules/nixos/hardware/gpu/nvidia/default.nix
+++ modules/nixos/hardware/gpu/nvidia/default.nix
@@ -8,6 +8,25 @@
 with lib;
 with lib.custom;
 let
+  #nvidiaPackage = config.boot.kernelPackages.nvidiaPackages.beta;
+  # TODO: use the latest beta version
+  nvidiaPackage = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+    version = "575.57.08";
+    sha256_64bit = "sha256-KqcB2sGAp7IKbleMzNkB3tjUTlfWBYDwj50o3R//xvI=";
+    sha256_aarch64 = "sha256-VJ5z5PdAL2YnXuZltuOirl179XKWt0O4JNcT8gUgO98=";
+    openSha256 = "sha256-DOJw73sjhQoy+5R0GHGnUddE6xaXb/z/Ihq3BKBf+lg=";
+    settingsSha256 = "sha256-AIeeDXFEo9VEKCgXnY3QvrW5iWZeIVg4LBCeRtMs5Io=";
+    persistencedSha256 = "sha256-Len7Va4HYp5r3wMpAhL4VsPu5S0JOshPFywbO7vYnGo=";
+
+    patches = [ gpl_symbols_linux_615_patch ];
+  };
+
+  gpl_symbols_linux_615_patch = pkgs.fetchpatch {
+    url = "https://github.com/CachyOS/kernel-patches/raw/914aea4298e3744beddad09f3d2773d71839b182/6.15/misc/nvidia/0003-Workaround-nv_vm_flags_-calling-GPL-only-code.patch";
+    hash = "sha256-YOTAvONchPPSVDP9eJ9236pAPtxYK5nAePNtm2dlvb4=";
+    stripLen = 1;
+    extraPrefix = "kernel/";
+  };
   cfg = config.hardware.gpu.nvidia;
 in
 {
@@ -29,8 +48,12 @@ in
     hardware.graphics = {
       enable = true;
       extraPackages = with pkgs; [
-        vaapiVdpau
+        libvdpau
+        libvdpau-va-gl
         nvidia-vaapi-driver
+        vdpauinfo
+        libva
+        libva-utils
       ];
     };
 
@@ -38,7 +61,10 @@ in
 
     hardware.nvidia = {
       prime = mkIf cfg.withIntegratedGPU {
-        offload.enable = true;
+        offload = {
+          enable = true;
+          enableOffloadCmd = true;
+        };
         intelBusId = cfg.intelBusID;
         nvidiaBusId = cfg.nvidiaBusID;
       };
@@ -48,7 +74,7 @@ in
       powerManagement.finegrained = true;
       open = true;
       nvidiaSettings = false;
-      package = config.boot.kernelPackages.nvidiaPackages.beta;
+      package = nvidiaPackage;
       dynamicBoost.enable = cfg.enable && cfg.withIntegratedGPU;
     };
 
@@ -85,12 +111,19 @@ in
       );
 
     boot = {
+      kernelModules = [
+        "nvidia"
+        "nvidia_modeset"
+        "nvidia_drm"
+        "nvidia_uvm"
+      ];
       extraModprobeConfig =
         "options nvidia "
         + lib.concatStringsSep " " [
           # nvidia assume that by default your CPU does not support PAT,
           # but this is effectively never the case in 2023
           "NVreg_UsePageAttributeTable=1"
+          "nvidia.NVreg_EnableGpuFirmware=1"
           # This is sometimes needed for ddc/ci support, see
           # https://www.ddcutil.com/nvidia/
           #
@@ -101,7 +134,17 @@ in
     };
 
     environment = {
-      systemPackages = [ pkgs.libva-utils ];
+      systemPackages = with pkgs; [
+        nvtopPackages.nvidia
+        libva-utils
+        gwe
+        vulkan-tools
+        zenith-nvidia
+        nvitop
+        vulkanPackages_latest.vulkan-loader
+        vulkanPackages_latest.vulkan-validation-layers
+        vulkanPackages_latest.vulkan-tools
+      ];
 
       variables = {
         # Required to run the correct GBM backend for nvidia GPUs on wayland
