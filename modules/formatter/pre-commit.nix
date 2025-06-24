e2dc5cdd31da928b1e8eab31a7f2bbeb3c78e4f9
Jason Bowman <jason@json64.dev>
2025-06-24 11:08:38 -0700

    add systems, re-organize formatter

---
+++ modules/formatter/pre-commit.nix
@@ -0,0 +1,19 @@
+{ inputs, ... }:
+{
+  imports = [
+    inputs.pre-commit-hooks.flakeModule
+    inputs.git-hooks-nix.flakeModule
+  ];
+
+  perSystem = _: {
+    pre-commit = {
+      check.enable = true;
+
+      settings.hooks = {
+        treefmt.enable = true;
+        statix.enable = true;
+      };
+    };
+
+  };
+}
