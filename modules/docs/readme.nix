28b0f908561795c34b9d5e2b5d8454607524f74e
Jason Bowman <jason@json64.dev>
2025-06-25 14:02:31 -0700

    update readme

--- modules/docs/readme.nix
+++ modules/docs/readme.nix
@@ -21,25 +21,25 @@
     parts.header =
       # markdown
       ''
-          <br>
+        <br>
 
-          # sini/nix-config
+        # sini/nix-config
 
-          <br>
-          <div align="center">
-              <a href="https://github.com/sini/nix-config/stargazers">
-                  <img src="https://img.shields.io/github/stars/sini/nix-config?color=c14d26&labelColor=0b0b0b&style=for-the-badge&logo=starship&logoColor=c14d26">
-              </a>
-              <a href="https://github.com/sini/nix-config">
-                  <img src="https://img.shields.io/github/repo-size/sini/nix-config?color=c14d26&labelColor=0b0b0b&style=for-the-badge&logo=github&logoColor=c14d26">
-              </a>
-              <a href="https://nixos.org">
-                  <img src="https://img.shields.io/badge/NixOS-unstable-blue.svg?style=for-the-badge&labelColor=0b0b0b&logo=NixOS&logoColor=c14d26&color=c14d26">
-              </a>
-              <a href="https://github.com/sini/nix-config/blob/main/LICENSE">
-                  <img src="https://img.shields.io/static/v1.svg?style=for-the-badge&label=License&message=MIT&colorA=0b0b0b&colorB=c14d26&logo=unlicense&logoColor=c14d26"/>
-              </a>
-          </div>
+        <br>
+        <div align="center">
+            <a href="https://github.com/sini/nix-config/stargazers">
+                <img src="https://img.shields.io/github/stars/sini/nix-config?color=c14d26&labelColor=0b0b0b&style=for-the-badge&logo=starship&logoColor=c14d26">
+            </a>
+            <a href="https://github.com/sini/nix-config">
+                <img src="https://img.shields.io/github/repo-size/sini/nix-config?color=c14d26&labelColor=0b0b0b&style=for-the-badge&logo=github&logoColor=c14d26">
+            </a>
+            <a href="https://nixos.org">
+                <img src="https://img.shields.io/badge/NixOS-unstable-blue.svg?style=for-the-badge&labelColor=0b0b0b&logo=NixOS&logoColor=c14d26&color=c14d26">
+            </a>
+            <a href="https://github.com/sini/nix-config/blob/main/LICENSE">
+                <img src="https://img.shields.io/static/v1.svg?style=for-the-badge&label=License&message=MIT&colorA=0b0b0b&colorB=c14d26&logo=unlicense&logoColor=c14d26"/>
+            </a>
+        </div>
 
         <br>
       '';
