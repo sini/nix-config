7026a24c71f6416261a97956792d2bdc619ad2ed
Jason Bowman <jason@json64.dev>
2025-06-30 13:37:58 -0700

    add media-data-share to server role

--- modules/hosts/axon-01/host.nix
+++ modules/hosts/axon-01/host.nix
@@ -1,12 +1,10 @@
-{ config, ... }:
 {
   flake.hosts.axon-01 = {
     deployment.targetHost = "10.10.10.2";
     roles = [
       "server"
     ];
-    extra_modules = with config.flake.modules.nixos; [
-      media-data-share
+    extra_modules = [
     ];
     public_key = ./ssh_host_ed25519_key.pub;
     facts = ./facter.json;
--- modules/hosts/axon-02/host.nix
+++ modules/hosts/axon-02/host.nix
@@ -1,12 +1,10 @@
-{ config, ... }:
 {
   flake.hosts.axon-02 = {
     deployment.targetHost = "10.10.10.3";
     roles = [
       "server"
     ];
-    extra_modules = with config.flake.modules.nixos; [
-      media-data-share
+    extra_modules = [
       ./_local
     ];
     public_key = ./ssh_host_ed25519_key.pub;
