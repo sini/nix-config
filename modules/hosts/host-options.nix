b4d21f336d82f48dcf9634f1a4917d67adf8e574
Jason Bowman <jason@json64.dev>
2025-06-30 13:21:54 -0700

    rename tag to role, auto-import roles

--- modules/hosts/host-options.nix
+++ modules/hosts/host-options.nix
@@ -20,7 +20,7 @@ in
       - `system`: The system architecture of the host (e.g., `x86_64-linux`).
       - `unstable`: Whether to use unstable packages for the host.
       - `deployment.targetHost`: The target host for deployment.
-      - `tags`: A list of tags for the host, which can be used to target
+      - `roles`: A list of tags for the host, which can be used to target
         specific hosts during deployment.
       - `public_key`: The path or value of the public SSH key for the host used for encryption.
       - `facts`: The path to the Facter JSON file for the host, which is used to provide
@@ -51,10 +51,10 @@ in
             };
           };
 
-          tags = mkOption {
+          roles = mkOption {
             type = types.listOf types.str;
             default = [ ];
-            description = "List of tags for the host.";
+            description = "List of roles for the host.";
           };
 
           public_key = mkOption {
