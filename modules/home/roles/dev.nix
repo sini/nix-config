e92fa94d20d8f0bb1f2bc447d97d215efbd2b795
Jason Bowman <jason@json64.dev>
2025-07-01 16:22:00 -0700

    add eza

--- modules/home/roles/dev.nix
+++ modules/home/roles/dev.nix
@@ -1,10 +1,12 @@
 { config, ... }:
 {
   flake.modules.nixos.home-dev = {
-    home-manager.users.${config.flake.meta.user.username}.imports = [
-      config.flake.modules.homeManager.direnv
-      config.flake.modules.homeManager.git
-      config.flake.modules.homeManager.gpg
-    ];
+    home-manager.users.${config.flake.meta.user.username}.imports =
+      with config.flake.modules.homeManager; [
+        direnv
+        eza
+        git
+        gpg
+      ];
   };
 }
