8145ad87e4bd8dce5eb487ee849b43fe114d0257
Jason Bowman <jason@json64.dev>
2025-07-01 16:39:10 -0700

    fix bat

--- modules/home/roles/dev.nix
+++ modules/home/roles/dev.nix
@@ -3,9 +3,9 @@
   flake.modules.nixos.home-dev = {
     home-manager.users.${config.flake.meta.user.username}.imports =
       with config.flake.modules.homeManager; [
-        bat
         direnv
         eza
+        file-tools
         git
         gpg
         yazi
