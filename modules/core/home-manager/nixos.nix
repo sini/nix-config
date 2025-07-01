41897aeb2b1de802e4c46fe15048308716afecd1
Jason Bowman <jason@json64.dev>
2025-07-01 14:59:41 -0700

    move direnv to home-ext

--- modules/core/home-manager/nixos.nix
+++ modules/core/home-manager/nixos.nix
@@ -3,27 +3,32 @@
   ...
 }:
 {
-  flake.modules.nixos.home-manager = {
-    # home-manager is defined in nixos-configurations for all machines
-    # which lets us switch the version used for stable, unstable, and
-    # darwin hosts.
-    # imports = [ inputs.home-manager.nixosModules.home-manager ];
+  flake.modules.nixos.home-manager =
+    { inputs, hostConfig, ... }:
+    {
+      # home-manager is defined in nixos-configurations for all machines
+      # which lets us switch the version used for stable, unstable, and
+      # darwin hosts.
+      # imports = [ inputs.home-manager.nixosModules.home-manager ];
 
-    home-manager = {
-      useGlobalPkgs = true;
-      extraSpecialArgs.hasGlobalPkgs = true;
+      home-manager = {
+        useGlobalPkgs = true;
+        extraSpecialArgs = {
+          inherit inputs hostConfig;
+          hasGlobalPkgs = true;
+        };
 
-      users.${config.flake.meta.user.username}.imports = [
-        (
-          { osConfig, ... }:
-          {
-            # TODO: Fix this to support nix-darwin
-            home.stateVersion = osConfig.system.stateVersion;
-          }
-        )
-        config.flake.modules.homeManager.core
-        # config.flake.modules.homeManager.gui
-      ];
+        users.${config.flake.meta.user.username}.imports = [
+          (
+            { osConfig, ... }:
+            {
+              # TODO: Fix this to support nix-darwin
+              home.stateVersion = osConfig.system.stateVersion;
+            }
+          )
+          config.flake.modules.homeManager.core
+          # config.flake.modules.homeManager.gui
+        ];
+      };
     };
-  };
 }
