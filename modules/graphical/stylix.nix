a239a391ef910d6bd88e1ecab69033de1538f746
Jason Bowman <jason@json64.dev>
2025-07-09 14:06:53 -0700

    import nixos stylix

--- modules/graphical/stylix.nix
+++ modules/graphical/stylix.nix
@@ -4,57 +4,60 @@
     nixos.stylix =
       { pkgs, ... }:
       {
-        stylix = {
-          enable = true;
-          autoEnable = true;
-          base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
-          homeManagerIntegration.autoImport = false;
-          homeManagerIntegration.followSystem = false;
+        imports = [ inputs.stylix.nixosModules.stylix ];
+        config = {
+          stylix = {
+            enable = true;
+            autoEnable = true;
+            base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
+            homeManagerIntegration.autoImport = false;
+            homeManagerIntegration.followSystem = false;
 
-          # image = pkgs.nixicle.wallpapers.windows-error;
+            # image = pkgs.nixicle.wallpapers.windows-error;
 
-          cursor = {
-            name = "Bibata-Modern-Classic";
-            package = pkgs.bibata-cursors;
-            size = 24;
+            cursor = {
+              name = "Bibata-Modern-Classic";
+              package = pkgs.bibata-cursors;
+              size = 24;
+            };
+
+            fonts = {
+              sizes = {
+                terminal = 14;
+                applications = 12;
+                popups = 12;
+              };
+
+              serif = {
+                name = "Source Serif";
+                package = pkgs.source-serif;
+              };
+
+              sansSerif = {
+                name = "Noto Sans";
+                package = pkgs.noto-fonts;
+              };
+
+              monospace = {
+                package = pkgs.nerd-fonts.dejavu-sans-mono;
+                name = "DejaVu Sans Mono";
+              };
+
+              emoji = {
+                package = pkgs.noto-fonts-emoji;
+                name = "Noto Color Emoji";
+              };
+            };
           };
 
-          fonts = {
-            sizes = {
-              terminal = 14;
-              applications = 12;
-              popups = 12;
-            };
+          home-manager.users.${config.flake.meta.user.username}.imports =
+            with config.flake.modules.homeManager; [
+              stylix
+              inputs.stylix.homeModules.stylix
+              inputs.catppuccin.homeModules.catppuccin
+            ];
 
-            serif = {
-              name = "Source Serif";
-              package = pkgs.source-serif;
-            };
-
-            sansSerif = {
-              name = "Noto Sans";
-              package = pkgs.noto-fonts;
-            };
-
-            monospace = {
-              package = pkgs.nerd-fonts.dejavu-sans-mono;
-              name = "DejaVu Sans Mono";
-            };
-
-            emoji = {
-              package = pkgs.noto-fonts-emoji;
-              name = "Noto Color Emoji";
-            };
-          };
         };
-
-        home-manager.users.${config.flake.meta.user.username}.imports =
-          with config.flake.modules.homeManager; [
-            stylix
-            inputs.stylix.homeModules.stylix
-            inputs.catppuccin.homeModules.catppuccin
-          ];
-
       };
 
     homeManager.stylix =
