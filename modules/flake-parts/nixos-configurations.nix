6c04953c5c3c6b2b924edc74cf42cbfc42420c6d
Jason Bowman <jason@json64.dev>
2025-07-09 16:12:56 -0700

    stash

--- modules/flake-parts/nixos-configurations.nix
+++ modules/flake-parts/nixos-configurations.nix
@@ -1,28 +1,23 @@
 {
-  self,
   inputs,
   withSystem,
   ...
 }:
 {
   flake =
-    {
-      config,
-      ...
-    }:
+    { config, ... }:
     let
-      lib = inputs.nixpkgs.lib;
+      nixpkgsLib = inputs.nixpkgs.lib;
       unstableLib = inputs.nixpkgs-unstable.lib;
-    in
-    {
-      nixosConfigurations = lib.mapAttrs (
+
+      hostDefs = nixpkgsLib.mapAttrs (
         hostname: hostOptions:
         withSystem hostOptions.system (
           { system, ... }:
           let
             nixpkgs' = if hostOptions.unstable then inputs.nixpkgs-unstable else inputs.nixpkgs;
             homeManager' = if hostOptions.unstable then inputs.home-manager-unstable else inputs.home-manager;
-            extendedLibrary = if hostOptions.unstable then unstableLib else lib;
+            extendedLibrary = if hostOptions.unstable then unstableLib else nixpkgsLib;
             chaotic_imports =
               if hostOptions.unstable then
                 [ inputs.chaotic.nixosModules.default ]
@@ -32,25 +27,18 @@
                   inputs.chaotic.nixosModules.nyx-overlay
                   inputs.chaotic.nixosModules.nyx-registry
                 ];
-          in
-          nixpkgs'.lib.nixosSystem {
-            inherit system;
-
-            specialArgs = {
-              inherit inputs hostOptions;
-              inherit (config) nodes;
-              lib = extendedLibrary;
-            };
 
             modules =
               [ config.modules.nixos.role_core ]
-              ++ (lib.optionals (hostOptions ? roles) (
+              ++ (nixpkgsLib.optionals (hostOptions ? roles) (
                 builtins.map (role: inputs.self.modules.nixos."role_${role}") (
-                  lib.filter (role: lib.hasAttr "role_${role}" inputs.self.modules.nixos) hostOptions.roles
+                  nixpkgsLib.filter (
+                    role: nixpkgsLib.hasAttr "role_${role}" inputs.self.modules.nixos
+                  ) hostOptions.roles
                 )
               ))
+              ++ hostOptions.extra_modules
               ++ chaotic_imports
-              ++ (hostOptions.extra_modules)
               ++ [
                 nixpkgs'.nixosModules.notDetected
                 homeManager'.nixosModules.home-manager
@@ -61,12 +49,47 @@
                   age.rekey.hostPubkey = hostOptions.public_key;
                 }
               ];
+
+            specialArgs = {
+              inherit inputs hostOptions;
+              inherit (config) nodes;
+              lib = extendedLibrary;
+            };
+
+            systemConfig = nixpkgs'.lib.nixosSystem {
+              inherit system modules specialArgs;
+            };
+          in
+          {
+            inherit systemConfig modules specialArgs;
           }
         )
       ) config.hosts;
 
-      # All nixosSystem instanciations are collected here, so that we can refer
-      # to any system via nodes.<name>
-      nodes = self.outputs.nixosConfigurations;
+      nixosConfigurations = nixpkgsLib.mapAttrs (_: v: v.systemConfig) hostDefs;
+    in
+    {
+      inherit nixosConfigurations;
+
+      nodes = nixosConfigurations;
+
+      colmena = {
+        meta =
+          {
+            nixpkgs = import inputs.nixpkgs-unstable {
+              system = "x86_64-linux";
+            };
+            nodeNixpkgs = builtins.mapAttrs (_: v: v.systemConfig.pkgs) hostDefs;
+            nodeSpecialArgs = builtins.mapAttrs (_: v: v.specialArgs) hostDefs;
+          }
+          // (nixpkgsLib.mapAttrs (hostname: v: {
+            imports = v.modules;
+            deployment = {
+              targetHost = config.hosts.${hostname}.deployment.targetHost;
+              tags = config.hosts.${hostname}.roles;
+              allowLocalDeployment = true;
+            };
+          }) hostDefs);
+      };
     };
 }
