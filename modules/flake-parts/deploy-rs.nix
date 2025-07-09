4a97c3280f4ad89e26af7f8eea3e754d41c25aee
Jason Bowman <jason@json64.dev>
2025-07-09 16:47:40 -0700

    stash

--- modules/flake-parts/deploy-rs.nix
+++ modules/flake-parts/deploy-rs.nix
@@ -11,14 +11,26 @@ in
   flake =
     { config, ... }:
     {
-      deploy.nodes = lib.mapAttrs (hostname: hostOptions: {
-        hostname = hostOptions.deployment.targetHost;
-        sshUser = hostOptions.deployment.sshUser or "root";
-        profiles.system = {
-          user = "root";
-          path = self.nixosConfigurations.${hostname}.config.system.build.toplevel;
-        };
-      }) config.hosts;
+      # deploy.nodes = lib.mapAttrs (hostname: hostOptions: {
+      #   hostname = hostOptions.deployment.targetHost;
+      #   sshUser = hostOptions.deployment.sshUser or "root";
+      #   profiles.system = {
+      #     user = "root";
+      #     path = self.nixosConfigurations.${hostname}.config.system.build.toplevel;
+      #   };
+      # }) config.hosts;
+
+      deploy.nodes = lib.mapAttrs (
+        hostname: options:
+        {
+          inherit hostname;
+          profiles.system = {
+            user = "root";
+            path = deploy-rs.lib.${options.system}.activate.nixos self.nixosConfigurations.${hostname};
+          };
+        }
+        // options.deploy
+      ) config.hosts;
     };
 
   perSystem =
