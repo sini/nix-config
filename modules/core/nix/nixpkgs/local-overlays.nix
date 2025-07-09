3ae90ee7252c8e0554d2ae99087f8931d167b004
Jason Bowman <jason@json64.dev>
2025-07-09 10:10:32 -0700

    stash pkg changes, change to by-name

--- modules/core/nix/nixpkgs/local-overlays.nix
+++ modules/core/nix/nixpkgs/local-overlays.nix
@@ -1,8 +1,25 @@
-{ inputs, rootPath, ... }:
+{
+  inputs,
+  withSystem,
+  rootPath,
+  ...
+}:
 {
   flake.modules.nixos.nixpkgs = {
     nixpkgs.overlays =
-      [ ]
+      [
+        # This brings our custom packages from the 'pkgs' directory under `pkgs.local`
+        # provided with inputs.pkgs-by-name-for-flake-parts.flakeModule
+        (
+          _final: prev:
+          withSystem prev.stdenv.hostPlatform.system (
+            { config, ... }:
+            {
+              local = config.packages;
+            }
+          )
+        )
+      ]
       ++ builtins.attrValues (
         import (rootPath + "/overlays/default.nix") {
           inherit inputs;
