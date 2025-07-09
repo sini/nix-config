ab2443f7ac84e540e361da347e2711cf08f9b5ad
Jason Bowman <jason@json64.dev>
2025-07-08 18:11:07 -0700

    update completioninit

--- modules/home/core/zsh.nix
+++ modules/home/core/zsh.nix
@@ -8,7 +8,6 @@
     {
       home.packages = with pkgs; [
         sqlite-interactive # For zsh-histdb
-        #zsh
         nix-zsh-completions
         fzy
       ];
@@ -22,13 +21,13 @@
         dotDir = ".config/zsh";
         autosuggestion.enable = true;
         enableCompletion = true;
+        completionInit = "autoload -U compinit && compinit -i";
         autocd = true;
         history = {
           path = "\${XDG_DATA_HOME-$HOME/.local/share}/zsh/history";
           ignoreSpace = true;
           size = 1000000;
         };
-
         plugins = [
           {
             # Must be before plugins that wrap widgets, such as zsh-autosuggestions or fast-syntax-highlighting
