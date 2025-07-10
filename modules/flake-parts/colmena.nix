ae47afebf2f1e9c3d3648b512f646c5ea0551f79
Jason Bowman <jason@json64.dev>
2025-07-10 10:56:56 -0700

    stash

--- modules/flake-parts/colmena.nix
+++ modules/flake-parts/colmena.nix
@@ -11,13 +11,15 @@
       config,
       ...
     }:
-    {
-      colmena =
+    let
+      stableHosts = lib.filterAttrs (_: h: !(h.unstable or false)) config.hosts;
+      # unstableHosts = lib.filterAttrs (_: h: h.unstable or false) config.hosts;
+      mkColmenaHive =
+        { hosts, nixpkgs }:
         lib.mapAttrs (
-          hostname: nixosConfig:
-          # For each NixOS configuration, we find its original options from the flake.
+          hostname: hostOptions:
           let
-            hostOptions = config.hosts.${hostname};
+            nixosConfig = self.nixosConfigurations.${hostname};
           in
           {
             imports = nixosConfig._module.args.modules;
@@ -27,15 +29,52 @@
               allowLocalDeployment = true;
             };
           }
-        ) self.nixosConfigurations
+        ) hosts
         // {
           meta = {
-            nixpkgs = import inputs.nixpkgs { system = "x86_64-linux"; };
-            # This is triggering twice:
-            # nodeNixpkgs = builtins.mapAttrs (_: v: v.pkgs) self.nixosConfigurations;
-            nodeSpecialArgs = builtins.mapAttrs (_: v: v._module.specialArgs) self.nixosConfigurations;
+            nixpkgs = import nixpkgs { system = "x86_64-linux"; };
+            nodeSpecialArgs = builtins.mapAttrs (
+              hostname: _: self.nixosConfigurations.${hostname}._module.specialArgs
+            ) hosts;
           };
         };
+
+    in
+    {
+      colmena = mkColmenaHive {
+        hosts = stableHosts;
+        nixpkgs = inputs.nixpkgs;
+      };
+
+      # colmenaUnstable = mkColmenaHive {
+      #   hosts = unstableHosts;
+      #   nixpkgs = inputs.nixpkgs-unstable;
+      # };
+
+      # colmena =
+      #   lib.mapAttrs (
+      #     hostname: nixosConfig:
+      #     # For each NixOS configuration, we find its original options from the flake.
+      #     let
+      #       hostOptions = config.hosts.${hostname};
+      #     in
+      #     {
+      #       imports = nixosConfig._module.args.modules;
+      #       deployment = {
+      #         targetHost = hostOptions.deployment.targetHost;
+      #         tags = hostOptions.roles;
+      #         allowLocalDeployment = true;
+      #       };
+      #     }
+      #   ) self.nixosConfigurations
+      #   // {
+      #     meta = {
+      #       nixpkgs = import inputs.nixpkgs { system = "x86_64-linux"; };
+      #       # This is triggering twice:
+      #       # nodeNixpkgs = builtins.mapAttrs (_: v: v.pkgs) self.nixosConfigurations;
+      #       nodeSpecialArgs = builtins.mapAttrs (_: v: v._module.specialArgs) self.nixosConfigurations;
+      #     };
+      #   };
 
       colmenaHive = inputs.colmena.lib.makeHive self.outputs.colmena;
     };
