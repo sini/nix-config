9eb40674c8fd327b14f80f58c5d4bfcd142d1a66
Jason Bowman <jason@json64.dev>
2025-07-10 08:35:40 -0700

    Revert "remove colmena"
    This reverts commit 32f394510e1b82f9570ae949544bfe252e4fe8d8.

---
+++ modules/flake-parts/colmena.nix
@@ -0,0 +1,45 @@
+{
+  self,
+  inputs,
+  ...
+}:
+{
+
+  flake =
+    {
+      lib,
+      config,
+      colmena,
+      ...
+    }:
+    {
+      colmena =
+        {
+          meta = {
+            nixpkgs = import inputs.nixpkgs-unstable {
+              system = "x86_64-linux";
+              # overlays = [ ];
+            };
+            nodeNixpkgs = builtins.mapAttrs (_: v: v.pkgs) self.nixosConfigurations;
+            nodeSpecialArgs = builtins.mapAttrs (_: v: v._module.specialArgs) self.nixosConfigurations;
+          };
+        }
+        // (lib.mapAttrs (
+          hostname: nixosConfig:
+          # For each NixOS configuration, we find its original options from the flake.
+          let
+            hostOptions = config.hosts.${hostname};
+          in
+          {
+            imports = nixosConfig._module.args.modules;
+            deployment = {
+              targetHost = hostOptions.deployment.targetHost;
+              tags = hostOptions.roles;
+              allowLocalDeployment = true;
+            };
+          }
+        ) self.nixosConfigurations);
+
+      colmenaHive = colmena.lib.makeHive self.outputs.colmena;
+    };
+}
