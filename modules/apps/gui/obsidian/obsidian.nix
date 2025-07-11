6dabc60b8e07c948c7562c6087e53b675c283b06
Jason Bowman <jason@json64.dev>
2025-07-11 14:20:07 -0700

    update obsidian

---
+++ modules/apps/gui/obsidian/obsidian.nix
@@ -0,0 +1,87 @@
+{
+  flake.modules.homeManager.obsidian =
+    { pkgs, ... }:
+    {
+      home.packages = with pkgs; [ obsidian ];
+      programs.obsidian = {
+        enable = true;
+        vaults = {
+          Obsidian = {
+            enable = true;
+            target = "Documents/obsidian";
+          };
+        };
+        defaultSettings = {
+          app = {
+            vimMode = true;
+            spellcheck = false;
+            showLineNumber = true;
+            propertiesInDocument = "hidden";
+          };
+          appearance = {
+            cssTheme = "Catppuccin";
+            theme = "obsidian";
+            showRibbon = false;
+            nativeMenus = false;
+            showInlineTitle = false;
+          };
+          corePlugins = [
+            "audio-recorder"
+            "backlink"
+            "bookmarks"
+            "canvas"
+            "command-palette"
+            "daily-notes"
+            "editor-status"
+            "file-explorer"
+            "file-recovery"
+            "global-search"
+            "graph"
+            "markdown-importer"
+            "note-composer"
+            "outgoing-link"
+            "outline"
+            "page-preview"
+            "properties"
+            "publish"
+            "random-note"
+            "slash-command"
+            "slides"
+            "switcher"
+            "sync"
+            "tag-pane"
+            "templates"
+            "word-count"
+            "workspaces"
+            "zk-prefixer"
+          ];
+
+          communityPlugins = [
+            {
+              pkg = pkgs.nur.obsidian-tasks;
+              enable = true;
+            }
+            {
+              pkg = pkgs.nur.obsidian-minimal-settings;
+              enable = true;
+            }
+            {
+              pkg = pkgs.nur.obsidian-dataview;
+              enable = true;
+            }
+          ];
+
+          hotkeys = {
+            "editor:delete-paragraph" = [ ];
+          };
+
+          cssSnippets = [
+            {
+              name = "stop-blinking-cursor";
+              source = ./_snippets/stop-blinking-cursor.css;
+            }
+          ];
+        };
+      };
+    };
+}
