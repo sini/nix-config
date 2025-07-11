759247add0d68eb289188e024da3b82df4111d45
Jason Bowman <jason@json64.dev>
2025-07-10 17:40:23 -0700

    remove pkgs override

--- modules/flake-parts/pkgs.nix
+++ modules/flake-parts/pkgs.nix
@@ -23,27 +23,25 @@
     )
   ];
 
-  perSystem =
-    { pkgs, system, ... }:
-    {
-      _module.args.pkgs = import inputs.nixpkgs-unstable {
-        inherit system;
-        config = {
-          allowUnfreePredicate = _pkg: true;
-        };
-        overlays =
-          builtins.attrValues (
-            import (rootPath + "/pkgs/overlays.nix") {
-              inherit inputs;
-            }
-          )
-          ++ [
-            inputs.nix-topology.overlays.default
-          ];
-      };
-      pkgsDirectory = rootPath + "/pkgs/by-name";
-      inherit pkgs;
-    };
+  perSystem = {
+    # _module.args.pkgs = import inputs.nixpkgs-unstable {
+    #   inherit system;
+    #   config = {
+    #     allowUnfreePredicate = _pkg: true;
+    #   };
+    #   overlays =
+    #     builtins.attrValues (
+    #       import (rootPath + "/pkgs/overlays.nix") {
+    #         inherit inputs;
+    #       }
+    #     )
+    #     ++ [
+    #       inputs.nix-topology.overlays.default
+    #     ];
+    # };
+    pkgsDirectory = rootPath + "/pkgs/by-name";
+    # inherit pkgs;
+  };
 
   flake = {
     overlays.default =
