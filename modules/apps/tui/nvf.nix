6dfd2536a8472c755c6d7d184934d489b780867b
Jason Bowman <jason@json64.dev>
2025-07-15 14:21:40 -0700

    update filetree settings

--- modules/apps/tui/nvf.nix
+++ modules/apps/tui/nvf.nix
@@ -137,6 +137,18 @@
               enable = true;
             };
           };
+          # Close filetree if it's the only window open
+          extraConfigLua = ''
+            vim.api.nvim_create_autocmd("BufEnter", {
+              nested = true,
+              callback = function()
+                if #vim.api.nvim_list_wins() == 1 and vim.bo.filetype == "NvimTree" then
+                  vim.cmd("quit")
+                end
+              end
+            })
+          '';
+
           tabline = {
             nvimBufferline.enable = true;
           };
