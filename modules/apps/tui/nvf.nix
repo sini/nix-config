09f4e2bd5a2d5374adb5d496a07a7e04b387d143
Jason Bowman <jason@json64.dev>
2025-07-15 14:27:18 -0700

    disable filetree

--- modules/apps/tui/nvf.nix
+++ modules/apps/tui/nvf.nix
@@ -132,22 +132,13 @@
           # General features
           autopairs.nvim-autopairs.enable = true;
           autocomplete.nvim-cmp.enable = true;
-          filetree = {
-            nvimTree = {
-              enable = true;
-            };
-          };
-          # Close filetree if it's the only window open
-          extraConfigLua = ''
-            vim.api.nvim_create_autocmd("BufEnter", {
-              nested = true,
-              callback = function()
-                if #vim.api.nvim_list_wins() == 1 and vim.bo.filetype == "NvimTree" then
-                  vim.cmd("quit")
-                end
-              end
-            })
-          '';
+
+          # Disable filetree for now
+          # filetree = {
+          #   nvimTree = {
+          #     enable = true;
+          #   };
+          # };
 
           tabline = {
             nvimBufferline.enable = true;
