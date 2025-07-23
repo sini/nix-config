ca2b7b80f62b83acc9707d4c66942568ce4ab8be
Jason Bowman <jason@json64.dev>
2025-07-23 15:56:42 -0700

    update lazygit and zathura

--- modules/apps/productivity/zathura.nix
+++ modules/apps/productivity/zathura.nix
@@ -4,6 +4,11 @@
       enable = true;
       # custom settings
       options = {
+        guioptions = "v";
+        adjust-open = "width";
+        statusbar-basename = true;
+        render-loading = false;
+        scroll-step = 120;
         selection-clipboard = "clipboard";
       };
     };
