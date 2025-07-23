7d19b84f7e585e09c618ee853b1766abc60ff8e1
Jason Bowman <jason@json64.dev>
2025-07-23 16:48:14 -0700

    add tlp

--- modules/apps/productivity/adb.nix
+++ modules/apps/productivity/adb.nix
@@ -1,9 +1,15 @@
 { config, ... }:
 {
-  flake.modules.nixos.adb = {
-    users.users.${config.flake.meta.user.username}.extraGroups = [
-      "adbusers"
-    ];
-    programs.adb.enable = true;
-  };
+  flake.modules.nixos.adb =
+    { pkgs, ... }:
+    {
+      users.users.${config.flake.meta.user.username}.extraGroups = [
+        "adbusers"
+      ];
+      programs.adb.enable = true;
+
+      services.udev.packages = [
+        pkgs.android-udev-rules
+      ];
+    };
 }
