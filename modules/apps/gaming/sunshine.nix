5da02955bad643867b8bdb73fb139356c07f35a2
Jason Bowman <jason@json64.dev>
2025-07-25 17:09:30 -0700

    stash app updates including: sunshine, coolercontrol

--- modules/apps/gaming/sunshine.nix
+++ modules/apps/gaming/sunshine.nix
@@ -1,74 +1,77 @@
+{ config, ... }:
+let
+  username = config.flake.meta.user.username;
+in
 {
-  flake.modules.homeManager.sunshine =
+  flake.modules.nixos.sunshine =
     { pkgs, ... }:
-
     {
-      programs.mangohud = {
-        enable = true;
-        enableSessionWide = true;
+      networking = {
+        firewall = {
+          allowedUDPPorts = [
+            # Moonlight
+            5353
+            47998
+            47999
+            48000
+            48002
+            48010
+          ];
+          allowedTCPPorts = [
+            # MoonDeck Buddy
+            59999
+            # Moonlight
+            47984
+            47989
+            48010
+          ];
+        };
       };
 
-      stylix.targets.mangohud.enable = false; # Overrides the config file
+      services.sunshine = {
+        enable = true;
+        autoStart = true;
+        capSysAdmin = true;
+        openFirewall = true;
+        applications = {
+          env = {
+            PATH = "$(PATH):/run/current-system/sw/bin:/etc/profiles/per-user/${username}/bin:$(HOME)/.local/bin";
+          };
+          apps = [
+            {
+              name = "Desktop";
+              image-path = "desktop.png";
+            }
+            {
+              name = "MoonDeckStream";
+              cmd = "${pkgs.local.moondeck-buddy}/bin/MoonDeckStream";
+              exclude-global-prep-cmd = "false";
+              elevated = "false";
+            }
+            {
+              name = "Steam Big Picture";
+              image-path = "steam.png";
+              detached = [ "steam steam://open/bigpicture" ];
+              auto-detach = "true";
+              wait-all = "true";
+              exit-timeout = "5";
+            }
+          ];
+        };
+        settings = {
+          output_name = 1;
+        };
+      };
 
-      # can't use the `programs.mangohud.settings` option as it sorts the keys, which changes the rendering order
-      # Colors are taken from https://github.com/catppuccin/mangohud/blob/main/themes/mocha/MangoHud.conf
-      xdg.configFile."MangoHud/MangoHud.conf".text = # conf
-        ''
-          # Hidden by default
-          no_display
+      home-manager.users.${username}.imports = with config.flake.modules.homeManager; [
+        sunshine
+      ];
+    };
 
-          # Text
-          font_size=14
-          font_file=${pkgs.aporetic}/share/fonts/truetype/aporetic-sans-mono-normalregularupright.ttf
-          text_color=CDD6F4
-          text_outline_color=313244
-
-          # Layout
-          horizontal
-          hud_compact
-          position=top-left
-          background_alpha=0.8
-          background_color=1E1E2E
-          round_corners=10
-
-          # Bindings
-          toggle_hud=Shift_R+F12
-          toggle_preset=Shift_R+F10
-
-          # Clock
-          time
-          time_no_label
-
-          # GPU
-          gpu_stats
-          gpu_temp
-          gpu_load_change
-          gpu_load_value=50,90
-          gpu_load_color=CDD6F4,FAB387,F38BA8
-          gpu_text=GPU
-          gpu_color=A6E3A1
-
-          # CPU
-          cpu_stats
-          cpu_temp
-          cpu_load_change
-          cpu_load_value=50,90
-          cpu_load_color=CDD6F4,FAB387,F38BA8
-          cpu_color=89B4FA
-          cpu_text=CPU
-          core_load_change
-
-          ## RAM ##
-          ram
-          ram_color=F5C2E7
-
-          # FPS
-          fps
-          fps_color_change=F38BA8,F9E2AF,A6E3A1
-
-          # Graph
-          frame_timing
-          frametime_color=A6E3A1
-        '';
+  flake.modules.homeManager.sunshine =
+    { pkgs, ... }:
+    {
+      home.packages = with pkgs.local; [ moondeck-buddy ];
+      xdg.autostart.entries = [ "${pkgs.local.moondeck-buddy}/share/applications/MoonDeckBuddy.desktop" ];
     };
 }
---
+++ pkgs/by-name/moondeck-buddy/package.nix
@@ -0,0 +1,66 @@
+# TODO: Rely on upstream once merged: https://github.com/NixOS/nixpkgs/pull/375287
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  nix-update-script,
+  kdePackages,
+  cmake,
+  ninja,
+  qt6,
+  procps,
+  xorg,
+  steam,
+  useNixSteam ? true,
+}:
+
+let
+  inherit (kdePackages) qtbase wrapQtAppsHook;
+  qtEnv =
+    with qt6;
+    env "qt-env-custom-${qtbase.version}" [
+      qthttpserver
+      qtwebsockets
+    ];
+in
+stdenv.mkDerivation (finalAttrs: {
+  pname = "moondeck-buddy";
+  version = "1.9.0";
+
+  src = fetchFromGitHub {
+    owner = "FrogTheFrog";
+    repo = "moondeck-buddy";
+    tag = "v${finalAttrs.version}";
+    fetchSubmodules = true;
+    hash = "sha256-txy0JEj7SCZlOJkIR1QgWtInoFG7hhrZttENaNx2bLQ=";
+  };
+
+  buildInputs = [
+    procps
+    xorg.libXrandr
+    qtbase
+    qtEnv
+  ];
+  nativeBuildInputs = [
+    cmake
+    ninja
+    wrapQtAppsHook
+  ];
+
+  postPatch = lib.optionalString useNixSteam ''
+    substituteInPlace src/lib/shared/appmetadata.cpp \
+      --replace-fail /usr/bin/steam ${lib.getExe steam};
+  '';
+
+  passthru.updateScript = nix-update-script { };
+
+  meta = {
+    mainProgram = "MoonDeckBuddy";
+    description = "Helper to work with moonlight on a steamdeck";
+    homepage = "https://github.com/FrogTheFrog/moondeck-buddy";
+    changelog = "https://github.com/FrogTheFrog/moondeck-buddy/releases/tag/v${finalAttrs.version}";
+    license = lib.licenses.lgpl3Only;
+    maintainers = with lib.maintainers; [ redxtech ];
+    platforms = lib.platforms.linux;
+  };
+})
