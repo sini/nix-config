aaec71a1a2a49b3f1f0a8a9bee6309e796cbb39a
Jason Bowman <jason@json64.dev>
2025-07-28 11:47:33 -0700

    add libvirt module

---
+++ modules/apps/productivity/virtualization.nix
@@ -0,0 +1,53 @@
+{ config, ... }:
+let
+  username = config.flake.meta.user.username;
+in
+{
+  flake.modules.nixos.virtualization =
+    { pkgs, ... }:
+    {
+      # Add user to libvirtd group
+      users.users.${username}.extraGroups = [ "libvirtd" ];
+
+      # Install necessary packages
+      environment.systemPackages = with pkgs; [
+        libguestfs
+        spice
+        spice-gtk
+        spice-protocol
+        virt-manager
+        virt-viewer
+        win-virtio
+        win-spice
+      ];
+
+      # Manage the virtualisation services
+      virtualisation = {
+        kvmgt.enable = true;
+        libvirtd = {
+          enable = true;
+          allowedBridges = [
+            "nm-bridge"
+            "virbr0"
+          ];
+          onBoot = "ignore";
+          onShutdown = "shutdown";
+          qemu = {
+            swtpm.enable = true;
+            ovmf = {
+              enable = true;
+              packages = [
+                (pkgs.OVMF.override {
+                  secureBoot = true;
+                  tpmSupport = true;
+                }).fd
+              ];
+            };
+          };
+        };
+        spiceUSBRedirection.enable = true;
+      };
+
+      services.spice-vdagentd.enable = true;
+    };
+}
