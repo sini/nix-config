b9866a20a0d7e0122cb56a01111b1301ef671bb6
Jason Bowman <jason@json64.dev>
2025-08-07 14:18:23 -0700

    disable vulkan-extension-layer for now

--- modules/hardware/gpu/amd.nix
+++ modules/hardware/gpu/amd.nix
@@ -29,7 +29,7 @@
             libvdpau-va-gl
             vulkanPackages_latest.vulkan-loader
             vulkanPackages_latest.vulkan-validation-layers
-            vulkanPackages_latest.vulkan-extension-layer
+            #vulkanPackages_latest.vulkan-extension-layer
             rocmPackages.clr.icd
           ];
         };
@@ -55,7 +55,7 @@
         vulkanPackages_latest.vulkan-tools
         vulkanPackages_latest.vulkan-loader
         vulkanPackages_latest.vulkan-validation-layers
-        vulkanPackages_latest.vulkan-extension-layer
+        #vulkanPackages_latest.vulkan-extension-layer
         libva-utils
         mesa-demos
       ];
