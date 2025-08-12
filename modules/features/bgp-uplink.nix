450365d6a0eded31d1fdbc0847e1d7e9484355ac
Jason Bowman <jason@json64.dev>
2025-08-12 15:52:42 -0700

    add podman to bitstream

---
+++ modules/features/bgp-uplink.nix
@@ -0,0 +1,44 @@
+#
+{
+  flake.modules.nixos.bgp-uplink = {
+    boot = {
+      kernel.sysctl = {
+        "net.ipv4.ip_forward" = 1;
+        "net.ipv4.conf.all.proxy_arp" = true;
+        "net.ipv6.conf.all.forwarding" = 1;
+        # These need to be increased for k8s
+        # Although the default settings might not cause issues initially, you'll get strange behavior after a while
+        "fs.inotify.max_user_instances" = 1048576;
+        "fs.inotify.max_user_watches" = 1048576;
+      };
+    };
+
+    services.frr = {
+      bgpd.enable = true;
+      config = ''
+        ip forwarding
+        ipv6 forwarding
+        !
+        ! FRR configuration for the 'uplink' machine
+        !
+        frr version 8.x
+        frr defaults traditional
+        hostname uplink
+        log stdout
+        router bgp 65001
+          ! Use the loopback IP of this machine as a stable router ID
+          bgp router-id 10.0.0.10
+          !
+          ! Define the three axon nodes as iBGP neighbors.
+          ! They are in the same AS (65001).
+          neighbor 10.10.10.2 remote-as 65001
+          neighbor 10.10.10.3 remote-as 65001
+          neighbor 10.10.10.4 remote-as 65001
+          !
+          ! This is the critical command to enable ECMP across the 3 links.
+          ! A value of 8 is a safe, common default.
+          maximum-paths 8
+      '';
+    };
+  };
+}
