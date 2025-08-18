e69ee4cdfb3e360931ec031d77a1c849545d501f
Jason Bowman <jason@json64.dev>
2025-08-18 15:52:39 -0700

    restore k3s

---
+++ modules/features/server-network.nix
@@ -0,0 +1,25 @@
+#
+{
+  flake.modules.nixos.server-network = {
+    boot.kernel.sysctl = {
+      "net.core.rmem_default" = 1048576;
+      "net.core.wmem_default" = 1048576;
+      "net.core.rmem_max" = 134217728;
+      "net.core.wmem_max" = 134217728;
+      "net.core.netdev_max_backlog" = 50000;
+      "net.core.netdev_budget" = 1000;
+      "net.ipv4.tcp_congestion_control" = "bbr";
+      "net.ipv4.route.max_size" = 524288;
+      "net.ipv4.tcp_fastopen" = "3";
+      "net.ipv6.conf.all.forwarding" = true;
+      "net.netfilter.nf_conntrack_max" = 131072;
+      "net.nf_conntrack_max" = 131072;
+      "net.ipv4.ip_forward" = 1;
+      "net.ipv4.conf.all.proxy_arp" = true;
+      # These need to be increased for k8s
+      # Although the default settings might not cause issues initially, you'll get strange behavior after a while
+      "fs.inotify.max_user_instances" = 1048576;
+      "fs.inotify.max_user_watches" = 1048576;
+    };
+  };
+}
