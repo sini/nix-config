ab33b5b0b98ef8572ff2208be9a401f2274a192a
Jason Bowman <jason@json64.dev>
2025-09-01 11:51:35 -0700

    move dark settings to stylix

--- modules/apps/desktop/stylix.nix
+++ modules/apps/desktop/stylix.nix
@@ -52,6 +52,18 @@ in
         ];
 
         config = {
+          gtk = {
+            enable = true;
+            gtk3.extraConfig.gtk-application-prefer-dark-theme = true;
+            gtk4.extraConfig.gtk-application-prefer-dark-theme = true;
+          };
+
+          dconf.settings = {
+            "org/gnome/desktop/interface" = {
+              color-scheme = "prefer-dark";
+            };
+          };
+
           stylix = {
             inherit base16Scheme;
 
