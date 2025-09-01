ea13c7fcc0421bf42fdfe4c3c379f1207f97396c
Jason Bowman <jason@json64.dev>
2025-09-01 14:51:51 -0700

    add fqdn to hosts

--- modules/core/homelab.nix
+++ modules/core/homelab.nix
@@ -1,7 +1,12 @@
 { config, lib, ... }:
 let
+  # TODO: Replace hardcoded FQDN with a scoped variable
   hosts = lib.attrsets.mapAttrs' (
-    hostname: hostConfig: (lib.attrsets.nameValuePair hostConfig.ipv4 [ hostname ])
+    hostname: hostConfig:
+    (lib.attrsets.nameValuePair hostConfig.ipv4 [
+      hostname
+      "${hostname}.json64.dev"
+    ])
   ) config.flake.hosts;
   sshKnownHosts = lib.attrsets.mapAttrs' (
     hostname: hostConfig:
