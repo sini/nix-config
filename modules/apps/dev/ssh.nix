2b838d9f40aef73ff163e46c1e1433249a35d9f8
Jason Bowman <jason@json64.dev>
2025-09-02 12:05:01 -0700

    add ssh matchblocks

--- modules/apps/dev/ssh.nix
+++ modules/apps/dev/ssh.nix
@@ -2,6 +2,31 @@
   flake.modules.homeManager.ssh = {
     programs.ssh = {
       enable = true;
+      enableDefaultConfig = false;
+      matchBlocks = {
+        "*" = {
+          forwardAgent = false;
+          # "a private key that is used during authentication will be added to ssh-agent if it is running"
+          addKeysToAgent = "yes";
+          compression = true;
+          serverAliveInterval = 0;
+          serverAliveCountMax = 3;
+          hashKnownHosts = false;
+          userKnownHostsFile = "~/.ssh/known_hosts";
+          controlMaster = "no";
+          controlPath = "~/.ssh/master-%r@%n:%p";
+          controlPersist = "no";
+        };
+        github = {
+          hostname = "github.com";
+          user = "git";
+        };
+        "10.10.*" = {
+          # "allow to securely use local SSH agent to authenticate on the remote machine."
+          # "It has the same effect as adding cli option `ssh -A user@host`"
+          forwardAgent = true;
+        };
+      };
     };
   };
 }
