8a58ba1bd79c96b640dcc462ff87d772206182e1
Jason Bowman <jason@json64.dev>
2025-09-02 12:05:01 -0700

    add ssh matchblocks

---
+++ modules/apps/desktop/xdg.nix
@@ -0,0 +1,27 @@
+{
+  flake.modules.homeManager.xdg =
+    {
+      pkgs,
+      ...
+    }:
+    {
+      home.packages = with pkgs; [
+        xdg-utils
+      ];
+
+      xdg = {
+        enable = true;
+        userDirs = {
+          enable = true;
+          desktop = "$HOME/desktop";
+          documents = "$HOME/documents";
+          download = "$HOME/downloads";
+          music = "$HOME/music";
+          pictures = "$HOME/pictures";
+          publicShare = "$HOME/public";
+          templates = "$HOME/documents/templates";
+          videos = "$HOME/videos";
+        };
+      };
+    };
+}
