c5481e4bf97aabe31691f40363b032dcf929f0ae
Jason Bowman <jason@json64.dev>
2025-09-04 12:18:18 -0700

    use beta instead of manually pinning pkg

--- modules/hardware/gpu/nvidia/nvidia-driver.nix
+++ modules/hardware/gpu/nvidia/nvidia-driver.nix
@@ -2,29 +2,9 @@
   flake.modules.nixos.gpu-nvidia-driver =
     {
       config,
-      lib,
       pkgs,
       ...
     }:
-    with lib;
-    let
-      nvidiaPackage = config.boot.kernelPackages.nvidiaPackages.mkDriver {
-        version = "580.65.06";
-        sha256_64bit = "sha256-BLEIZ69YXnZc+/3POe1fS9ESN1vrqwFy6qGHxqpQJP8=";
-        sha256_aarch64 = "sha256-4CrNwNINSlQapQJr/dsbm0/GvGSuOwT/nLnIknAM+cQ=";
-        openSha256 = "sha256-BKe6LQ1ZSrHUOSoV6UCksUE0+TIa0WcCHZv4lagfIgA=";
-        settingsSha256 = "sha256-9PWmj9qG/Ms8Ol5vLQD3Dlhuw4iaFtVHNC0hSyMCU24=";
-        persistencedSha256 = "sha256-ETRfj2/kPbKYX1NzE0dGr/ulMuzbICIpceXdCRDkAxA=";
-        #patches = [ gpl_symbols_linux_615_patch ];
-      };
-
-      # gpl_symbols_linux_615_patch = pkgs.fetchpatch {
-      #   url = "https://github.com/CachyOS/kernel-patches/raw/914aea4298e3744beddad09f3d2773d71839b182/6.15/misc/nvidia/0003-Workaround-nv_vm_flags_-calling-GPL-only-code.patch";
-      #   hash = "sha256-YOTAvONchPPSVDP9eJ9236pAPtxYK5nAePNtm2dlvb4=";
-      #   stripLen = 1;
-      #   extraPrefix = "kernel/";
-      # };
-    in
     {
       services.xserver.videoDrivers = [ "nvidia" ];
 
@@ -48,7 +28,7 @@
         open = true;
         nvidiaSettings = false;
         nvidiaPersistenced = true;
-        package = nvidiaPackage;
+        package = config.boot.kernelPackages.nvidiaPackages.beta;
       };
     };
 
