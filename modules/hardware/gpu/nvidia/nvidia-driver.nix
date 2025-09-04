97524be843f4be07476152765b73f112f1b37d6f
Jason Bowman <jason@json64.dev>
2025-09-04 11:53:32 -0700

    update nvidia driver version

--- modules/hardware/gpu/nvidia/nvidia-driver.nix
+++ modules/hardware/gpu/nvidia/nvidia-driver.nix
@@ -8,24 +8,22 @@
     }:
     with lib;
     let
-      # TODO: use the latest beta version
       nvidiaPackage = config.boot.kernelPackages.nvidiaPackages.mkDriver {
-        version = "575.64.05";
-        sha256_64bit = "sha256-hfK1D5EiYcGRegss9+H5dDr/0Aj9wPIJ9NVWP3dNUC0=";
-        sha256_aarch64 = "sha256-GRE9VEEosbY7TL4HPFoyo0Ac5jgBHsZg9sBKJ4BLhsA=";
-        openSha256 = "sha256-mcbMVEyRxNyRrohgwWNylu45vIqF+flKHnmt47R//KU=";
-        settingsSha256 = "sha256-o2zUnYFUQjHOcCrB0w/4L6xI1hVUXLAWgG2Y26BowBE=";
-        persistencedSha256 = "sha256-2g5z7Pu8u2EiAh5givP5Q1Y4zk4Cbb06W37rf768NFU=";
-
-        patches = [ gpl_symbols_linux_615_patch ];
+        version = "580.65.06";
+        sha256_64bit = "sha256-BLEIZ69YXnZc+/3POe1fS9ESN1vrqwFy6qGHxqpQJP8=";
+        sha256_aarch64 = "sha256-4CrNwNINSlQapQJr/dsbm0/GvGSuOwT/nLnIknAM+cQ=";
+        openSha256 = "sha256-BKe6LQ1ZSrHUOSoV6UCksUE0+TIa0WcCHZv4lagfIgA=";
+        settingsSha256 = "sha256-9PWmj9qG/Ms8Ol5vLQD3Dlhuw4iaFtVHNC0hSyMCU24=";
+        persistencedSha256 = "sha256-ETRfj2/kPbKYX1NzE0dGr/ulMuzbICIpceXdCRDkAxA=";
+        #patches = [ gpl_symbols_linux_615_patch ];
       };
 
-      gpl_symbols_linux_615_patch = pkgs.fetchpatch {
-        url = "https://github.com/CachyOS/kernel-patches/raw/914aea4298e3744beddad09f3d2773d71839b182/6.15/misc/nvidia/0003-Workaround-nv_vm_flags_-calling-GPL-only-code.patch";
-        hash = "sha256-YOTAvONchPPSVDP9eJ9236pAPtxYK5nAePNtm2dlvb4=";
-        stripLen = 1;
-        extraPrefix = "kernel/";
-      };
+      # gpl_symbols_linux_615_patch = pkgs.fetchpatch {
+      #   url = "https://github.com/CachyOS/kernel-patches/raw/914aea4298e3744beddad09f3d2773d71839b182/6.15/misc/nvidia/0003-Workaround-nv_vm_flags_-calling-GPL-only-code.patch";
+      #   hash = "sha256-YOTAvONchPPSVDP9eJ9236pAPtxYK5nAePNtm2dlvb4=";
+      #   stripLen = 1;
+      #   extraPrefix = "kernel/";
+      # };
     in
     {
       services.xserver.videoDrivers = [ "nvidia" ];
