162c9384fb5196907a207f5179c41f02823ad6eb
Jason Bowman <jason@json64.dev>
2025-09-05 15:18:12 -0700

    update env file

--- modules/features/bgp-uplink.nix
+++ modules/features/bgp-uplink.nix
@@ -13,7 +13,6 @@
       bgpd.enable = true;
       config = ''
         ip forwarding
-        ipv6 forwarding
         !
         ! FRR configuration for the 'uplink' machine
         !
@@ -21,15 +20,17 @@
         frr defaults traditional
         hostname uplink
         log stdout
-        router bgp 65001
+        router bgp 65000
           ! Use the loopback IP of this machine as a stable router ID
-          bgp router-id 10.0.0.10
+          bgp router-id 10.10.10.1
+          no bgp ebgp-requires-policy
+          bgp bestpath as-path multipath-relax
           !
           ! Define the three axon nodes as iBGP neighbors.
           ! They are in the same AS (65001).
           neighbor 10.10.10.2 remote-as 65001
-          neighbor 10.10.10.3 remote-as 65001
-          neighbor 10.10.10.4 remote-as 65001
+          neighbor 10.10.10.3 remote-as 65002
+          neighbor 10.10.10.4 remote-as 65003
           !
           ! This is the critical command to enable ECMP across the 3 links.
           ! A value of 8 is a safe, common default.
