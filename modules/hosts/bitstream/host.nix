8aa7cc2c5ee316c7a36aeb34803fb8033f90ce04
Jason Bowman <jason@json64.dev>
2025-09-17 10:58:18 -0700

    disable bond on bitstream

--- modules/hosts/bitstream/host.nix
+++ modules/hosts/bitstream/host.nix
@@ -34,58 +34,61 @@
           swap_size = 8192;
         };
         networking = {
-          interfaces = [ ];
-          unmanagedInterfaces = [
+          interfaces = [
             "eno1"
             "enp2s0"
-            "bond0"
           ];
+          # unmanagedInterfaces = [
+          #   "eno1"
+          #   "enp2s0"
+          #   "bond0"
+          # ];
         };
       };
 
-      systemd.network = {
-        netdevs = {
-          "10-bond0" = {
-            netdevConfig = {
-              Kind = "bond";
-              Name = "bond0";
-            };
-            bondConfig = {
-              # Mode = "balance-alb";
-              Mode = "balance-xor";
-              TransmitHashPolicy = "layer3+4";
-            };
-          };
-        };
+      # systemd.network = {
+      #   netdevs = {
+      #     "10-bond0" = {
+      #       netdevConfig = {
+      #         Kind = "bond";
+      #         Name = "bond0";
+      #       };
+      #       bondConfig = {
+      #         # Mode = "balance-alb";
+      #         Mode = "balance-xor";
+      #         TransmitHashPolicy = "layer3+4";
+      #       };
+      #     };
+      #   };
 
-        # Configure Bonds to utilize both 2.5Gbps ports
-        networks = {
-          "30-eno1" = {
-            enable = true;
-            matchConfig.PermanentMACAddress = "84:47:09:40:d5:f5";
-            networkConfig.Bond = "bond0";
-          };
+      #   # Configure Bonds to utilize both 2.5Gbps ports
+      #   networks = {
+      #     "30-eno1" = {
+      #       enable = true;
+      #       matchConfig.PermanentMACAddress = "84:47:09:40:d5:f5";
+      #       networkConfig.Bond = "bond0";
+      #     };
 
-          "30-enp2s0" = {
-            enable = true;
-            matchConfig.PermanentMACAddress = "84:47:09:40:d5:f4";
-            networkConfig.Bond = "bond0";
-          };
+      #     "30-enp2s0" = {
+      #       enable = true;
+      #       matchConfig.PermanentMACAddress = "84:47:09:40:d5:f4";
+      #       networkConfig.Bond = "bond0";
+      #     };
 
-          "40-bond0" = {
-            enable = true;
-            matchConfig.Name = "bond0";
-            networkConfig = {
-              DHCP = true;
-              LinkLocalAddressing = "no";
-            };
-            linkConfig = {
-              RequiredForOnline = "routable";
-              MACAddress = "84:47:09:40:d5:f4";
-            };
-          };
-        };
-      };
+      #     "40-bond0" = {
+      #       enable = true;
+      #       matchConfig.Name = "bond0";
+      #       networkConfig = {
+      #         DHCP = true;
+      #         LinkLocalAddressing = "no";
+      #       };
+      #       linkConfig = {
+      #         RequiredForOnline = "routable";
+      #         MACAddress = "84:47:09:40:d5:f4";
+      #       };
+      #     };
+      #   };
+      # };
 
       system.stateVersion = "25.05";
     };
