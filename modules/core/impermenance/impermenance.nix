4de0b939384d9cb451223c7d6eb2276cfbbc9415
Jason Bowman <jason@json64.dev>
2025-09-23 14:07:51 -0700

    stash

---
+++ modules/core/impermenance/impermenance.nix
@@ -0,0 +1,36 @@
+{
+  flake.modules.nixos.impermenance =
+    { lib, config, ... }:
+    {
+      environment.persistence = {
+        "/persist" = {
+          directories = [
+            "/var/lib/systemd"
+            "/var/lib/nixos"
+            "/var/log"
+          ];
+          files = [
+            "/etc/machine-id"
+            "/etc/adjtime"
+          ];
+        };
+      };
+      systemd.suppressedSystemUnits = [ "systemd-machine-id-commit.service" ];
+
+      # Needed for home-manager's impermanence allowOther option to work
+      programs.fuse.userAllowOther = true;
+
+      system.activationScripts.persistent-dirs.text =
+        let
+          mkHomePersist =
+            user:
+            lib.optionalString user.createHome ''
+              mkdir -p /persist/${user.home}
+              chown ${user.name}:${user.group} /persist/${user.home}
+              chmod ${user.homeMode} /persist/${user.home}
+            '';
+          users = lib.attrValues config.users.users;
+        in
+        lib.concatLines (map mkHomePersist users);
+    };
+}
