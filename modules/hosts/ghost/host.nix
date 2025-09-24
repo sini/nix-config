d00fb834e5ee859424ac4b9b03c87f29f8ff5d87
Jason Bowman <jason@json64.dev>
2025-09-23 18:31:20 -0700

    add wifi support to ghost

--- modules/hosts/ghost/host.nix
+++ modules/hosts/ghost/host.nix
@@ -1,4 +1,4 @@
-{ config, ... }:
+{ config, rootPath, ... }:
 {
   flake.hosts.ghost = {
     ipv4 = [
@@ -26,7 +26,49 @@
         ...
       }:
       {
+        age.secrets.wpa-supplicant = {
+          rekeyFile = rootPath + "/.secrets/user/wpa_supplicant-arcade.age";
+        };
+
+        age.secrets.wpa-supplicant-config = {
+          rekeyFile = rootPath + "/.secrets/user/wpa_supplicant-config.age";
+          path = "/root/.wpa_supplicant/wpa_supplicant-wlp1s0.conf";
+          symlink = false;
+        };
         boot.kernelPackages = pkgs.linuxPackages_cachyos-gcc; # TODO: https://github.com/chaotic-cx/nyx/issues/1178
+        boot.initrd = {
+          availableKernelModules = [
+            "ccm"
+            "ctr"
+            "iwlmvm"
+            "iwlwifi"
+          ];
+          systemd = {
+            packages = [ pkgs.wpa_supplicant ];
+            initrdBin = [ pkgs.wpa_supplicant ];
+            targets.initrd.wants = [ "wpa_supplicant@wlp1s0.service" ];
+            services."wpa_supplicant@".unitConfig.DefaultDependencies = false;
+          };
+          secrets."/etc/wpa_supplicant/wpa_supplicant-wlp1s0.conf" =
+            /root/.wpa_supplicant/wpa_supplicant-wlp1s0.conf;
+        };
+
+        systemd.network = {
+          networks."10-wlan" = {
+            enable = true;
+            matchConfig.Name = "wlp1s0";
+            DHCP = "yes";
+          };
+        };
+
+        networking.wireless = {
+          enable = true;
+          interfaces = [ "wlp1s0" ];
+          secretsFile = config.age.secrets.wpa-supplicant.path;
+          networks = {
+            "The Arcade".pskRaw = "ext:psk_arcade";
+          };
+        };
         system.stateVersion = "25.05";
       };
   };
