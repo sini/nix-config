f2b86303690cf4e53db47c655c348c498314f0b8
Jason Bowman <jason@json64.dev>
2025-09-23 18:06:07 -0700

    add wireless config to shell

--- modules/hosts/shell/host.nix
+++ modules/hosts/shell/host.nix
@@ -1,4 +1,4 @@
-{ config, ... }:
+{ config, rootPath, ... }:
 {
   flake.hosts.shell = {
     ipv4 = [
@@ -22,11 +22,52 @@
     facts = ./facter.json;
     nixosConfiguration =
       {
+        config,
         pkgs,
         ...
       }:
       {
+        age.secrets.wpa-supplicant = {
+          rekeyFile = rootPath + "/.secrets/user/wpa_supplicant-arcade.age";
+        };
+        age.secrets.wpa-supplicant-config = {
+          rekeyFile = rootPath + "/.secrets/user/wpa_supplicant-config.age";
+          path = "/root/.wpa_supplicant/wpa_supplicant-wlp3s0.conf";
+          symlink = false;
+        };
         boot.kernelPackages = pkgs.linuxPackages_cachyos-gcc; # TODO: https://github.com/chaotic-cx/nyx/issues/1178
+        boot.initrd = {
+          availableKernelModules = [
+            "ccm"
+            "ctr"
+            "iwlmvm"
+            "iwlwifi"
+          ];
+          systemd = {
+            packages = [ pkgs.wpa_supplicant ];
+            initrdBin = [ pkgs.wpa_supplicant ];
+            targets.initrd.wants = [ "wpa_supplicant@wlp3s0.service" ];
+            services."wpa_supplicant@".unitConfig.DefaultDependencies = false;
+          };
+          secrets."/etc/wpa_supplicant/wpa_supplicant-wlp3s0.conf" =
+            /root/.wpa_supplicant/wpa_supplicant-wlp3s0.conf;
+        };
+
+        systemd.network = {
+          networks."10-wlan" = {
+            enable = true;
+            matchConfig.Name = "wlp3s0";
+            DHCP = "yes";
+          };
+        };
+        networking.wireless = {
+          enable = true;
+          interfaces = [ "wlp3s0" ];
+          secretsFile = config.age.secrets.wpa-supplicant.path;
+          networks = {
+            "The Arcade".pskRaw = "ext:psk_arcade";
+          };
+        };
         system.stateVersion = "25.05";
       };
   };
