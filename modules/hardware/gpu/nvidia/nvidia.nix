a8e2d74b88d2460458634abf38431375d9074034
Jason Bowman <jason@json64.dev>
2025-10-01 16:43:35 -0700

    use requires feature

--- modules/hardware/gpu/nvidia/nvidia.nix
+++ modules/hardware/gpu/nvidia/nvidia.nix
@@ -1,77 +1,78 @@
-{ config, ... }:
 {
-  flake.aspects.gpu-nvidia.nixos =
-    {
-      pkgs,
-      ...
-    }:
-    {
-      imports = with config.flake.modules.nixos; [
-        gpu-nvidia-driver
-        gpu-nvidia-kernel
-      ];
+  flake.aspects.gpu-nvidia = {
+    requires = [
+      "gpu-nvidia-driver"
+      "gpu-nvidia-kernel"
+    ];
 
-      services.xserver.videoDrivers = [ "nvidia" ];
+    nixos =
+      {
+        pkgs,
+        ...
+      }:
+      {
 
-      nix.settings = {
-        substituters = [
-          "https://cuda-maintainers.cachix.org"
-        ];
-        trusted-public-keys = [
-          "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
-        ];
-      };
+        services.xserver.videoDrivers = [ "nvidia" ];
 
-      # TODO: move to cuda module...
-      nixpkgs.config = {
-        nvidia.acceptLicense = true;
-        cudaSupport = true;
-        cudnnSupport = true;
-      };
-
-      environment = {
-        systemPackages = with pkgs; [
-          lact # GUI for overclocking, undervolting, setting fan curves, etc.
-          pciutils
-          nvtopPackages.full
-          libva-utils
-          gwe
-          vulkan-tools
-          mesa-demos
-          zenith-nvidia
-          nvitop
-          btop-cuda
-          vulkanPackages_latest.vulkan-loader
-          vulkanPackages_latest.vulkan-validation-layers # From unstable
-          vulkanPackages_latest.vulkan-validation-layers
-          vulkanPackages_latest.vulkan-tools
-          cudaPackages.cudatoolkit
-          cudaPackages.cudnn
-          cudaPackages.cutensor
-          cudaPackages.cuda_cudart
-          cudaPackages.cuda_nvrtc
-          cudaPackages.cuda_nvcc
-          cudaPackages.cuda_nvtx
-          cudaPackages.cuda_nvml_dev
-        ];
-
-        variables = {
-          # TODO: Are these deprecated? Remove?
-          # Required to run the correct GBM backend for nvidia GPUs on wayland
-          #GBM_BACKEND = "nvidia-drm";
-          # Apparently, without this nouveau may attempt to be used instead
-          # (despite it being blacklisted)
-          #__GLX_VENDOR_LIBRARY_NAME = "nvidia";
-          # Hardware cursors are currently broken on wlroots
-          WLR_NO_HARDWARE_CURSORS = "1";
-          CUDA_PATH = "${pkgs.cudatoolkit}";
+        nix.settings = {
+          substituters = [
+            "https://cuda-maintainers.cachix.org"
+          ];
+          trusted-public-keys = [
+            "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
+          ];
         };
+
+        # TODO: move to cuda module...
+        nixpkgs.config = {
+          nvidia.acceptLicense = true;
+          cudaSupport = true;
+          cudnnSupport = true;
+        };
+
+        environment = {
+          systemPackages = with pkgs; [
+            lact # GUI for overclocking, undervolting, setting fan curves, etc.
+            pciutils
+            nvtopPackages.full
+            libva-utils
+            gwe
+            vulkan-tools
+            mesa-demos
+            zenith-nvidia
+            nvitop
+            btop-cuda
+            vulkanPackages_latest.vulkan-loader
+            vulkanPackages_latest.vulkan-validation-layers # From unstable
+            vulkanPackages_latest.vulkan-validation-layers
+            vulkanPackages_latest.vulkan-tools
+            cudaPackages.cudatoolkit
+            cudaPackages.cudnn
+            cudaPackages.cutensor
+            cudaPackages.cuda_cudart
+            cudaPackages.cuda_nvrtc
+            cudaPackages.cuda_nvcc
+            cudaPackages.cuda_nvtx
+            cudaPackages.cuda_nvml_dev
+          ];
+
+          variables = {
+            # TODO: Are these deprecated? Remove?
+            # Required to run the correct GBM backend for nvidia GPUs on wayland
+            #GBM_BACKEND = "nvidia-drm";
+            # Apparently, without this nouveau may attempt to be used instead
+            # (despite it being blacklisted)
+            #__GLX_VENDOR_LIBRARY_NAME = "nvidia";
+            # Hardware cursors are currently broken on wlroots
+            WLR_NO_HARDWARE_CURSORS = "1";
+            CUDA_PATH = "${pkgs.cudatoolkit}";
+          };
+        };
+
+        # TODO: Move to OC module...
+        # GPU overclocking/undervolting daemon
+        systemd.packages = with pkgs; [ lact ];
+        systemd.services.lactd.wantedBy = [ "multi-user.target" ];
       };
-
-      # TODO: Move to OC module...
-      # GPU overclocking/undervolting daemon
-      systemd.packages = with pkgs; [ lact ];
-      systemd.services.lactd.wantedBy = [ "multi-user.target" ];
-    };
-
+  };
 }
