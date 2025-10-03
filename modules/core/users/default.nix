95e0994983c5a96861932be44d3ee3d460c6b5ef
Jason Bowman <jason@json64.dev>
2025-10-03 15:40:17 -0700

    merge user configuration scopes

--- modules/core/users/default.nix
+++ modules/core/users/default.nix
@@ -1,5 +1,46 @@
 {
-  flake.features.users.nixos = {
-    users.mutableUsers = false;
-  };
+  flake.features.users.nixos =
+    {
+      users,
+      hostOptions,
+      environment,
+      ...
+    }:
+    let
+      # Users are already filtered in specialArgs, so we just collect all user configurations
+      enabledUsers = builtins.attrNames users;
+
+      # Function to build merged user configuration from all sources
+      buildUserConfig =
+        userName:
+        let
+          # Get user from flake.users
+          baseUser = users.${userName} or { };
+          baseConfig = baseUser.configuration or { };
+
+          # Get user from environment
+          envUser = environment.users.${userName} or { };
+          envConfig = envUser.configuration or { };
+
+          # Get user from host
+          hostUser = hostOptions.users.${userName} or { };
+          hostConfig = hostUser.configuration or { };
+
+          # Merge all configurations - later configs can override earlier ones
+          mergedConfig = {
+            imports = [
+              baseConfig
+              envConfig
+              hostConfig
+            ];
+          };
+        in
+        mergedConfig;
+
+      userConfigs = builtins.map buildUserConfig enabledUsers;
+    in
+    {
+      imports = userConfigs;
+      users.mutableUsers = false;
+    };
 }
