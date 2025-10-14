e7054f43dfaf2bb1f345eb75f0d870b8ece4b4d1
Jason Bowman <jason@json64.dev>
2025-10-14 14:13:08 -0700

    can we use nvidia prime on a desktop with vfio?

--- modules/core/facter/facter.nix
+++ modules/core/facter/facter.nix
@@ -4,6 +4,7 @@
     nixos = {
       imports = [ inputs.nixos-facter-modules.nixosModules.facter ];
       facter.detected.dhcp.enable = false;
+      facter.detected.graphics.enable = false; # Don't configure graphics, we'll do that...
     };
 
     home =
