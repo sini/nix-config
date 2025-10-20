0657ec7bdd6d2e8276753dd30281110ab37a54f6
Jason Bowman <jason@json64.dev>
2025-10-20 14:09:35 -0700

    update impermenance to support legacy fs settings

--- modules/core/systemd/logging.nix
+++ modules/core/systemd/logging.nix
@@ -1,20 +1,32 @@
 {
-  flake.features.systemd.nixos = {
-    systemd.tmpfiles.rules = [
-      # cleanup systemd coredumps once a week
-      "d /var/lib/systemd/coredump 0755 root root 7d"
-    ];
+  flake.features.systemd.nixos =
+    {
+      lib,
+      activeFeatures,
+      ...
+    }:
+    let
+      legacyFs = lib.elem "disk-single" activeFeatures;
+    in
+    {
+      systemd.tmpfiles.rules = [
+        # cleanup systemd coredumps once a week
+        "d /var/lib/systemd/coredump 0755 root root 7d"
+      ];
 
-    environment.persistence."/volatile".directories = [
-      "/var/lib/systemd/coretdump"
-      "/var/lib/systemd/timers"
-      "/var/log"
-    ];
+      environment.persistence."/volatile".directories = [
+        "/var/lib/systemd/coredump"
+        "/var/lib/systemd/timers"
+      ]
+      ++
+        # If using legacy disk-configuration, don't persist logs as they are on their own subvolume
+        #TODO: Remove once we kill legacyFs support
+        lib.optional (!legacyFs) "/var/log";
 
-    # Limit logging to 90 days or 2gb
-    services.journald.extraConfig = ''
-      MaxRetentionSec=3month
-      SystemMaxUse=2G
-    '';
-  };
+      # Limit logging to 90 days or 2gb
+      services.journald.extraConfig = ''
+        MaxRetentionSec=3month
+        SystemMaxUse=2G
+      '';
+    };
 }
