8043fc7c2fba2faaaef61e6d114ec96aab1fa7cd
Jason Bowman <jason@json64.dev>
2025-10-20 11:04:32 -0700

    stash blade refactor

---
+++ modules/hosts/_spike/host.nix
@@ -0,0 +1,38 @@
+{ ... }:
+{
+  flake.hosts.spike = {
+    ipv4 = [ "10.9.3.1" ];
+    ipv6 = [ "2001:5a8:608c:4a00::31/64" ];
+    environment = "dev";
+    roles = [
+      "workstation"
+      "laptop"
+      "gaming"
+      "dev"
+      "dev-gui"
+      "media"
+    ];
+    features = [
+      "cpu-intel"
+      "gpu-intel"
+      "gpu-nvidia"
+      "gpu-nvidia-prime"
+      "razer"
+    ];
+    extra_modules = [
+      ./_local
+    ];
+    users = {
+      "sini" = {
+        "features" = [
+          "spotify-player"
+        ];
+      };
+    };
+    facts = ./facter.json;
+    nixosConfiguration = {
+      # Enable NetworkManager for managing network interfaces
+      networking.networkmanager.enable = true;
+    };
+  };
+}
---
+++ modules/hosts/blade/host.nix
@@ -0,0 +1,46 @@
+{ ... }:
+{
+  flake.hosts.spike = {
+    ipv4 = [ "10.9.3.1" ];
+    ipv6 = [ "2001:5a8:608c:4a00::31/64" ];
+    environment = "dev";
+    roles = [
+      "workstation"
+      "laptop"
+      "gaming"
+      "dev"
+      "dev-gui"
+      "media"
+    ];
+    features = [
+      "cpu-intel"
+      "gpu-intel"
+      "gpu-nvidia"
+      "gpu-nvidia-prime"
+      "disk-single"
+      "network-manager"
+      "razer"
+    ];
+    users = {
+      "sini" = {
+        "features" = [
+          "spotify-player"
+        ];
+      };
+    };
+    facts = ./facter.json;
+    nixosConfiguration =
+      { pkgs, ... }:
+      {
+        boot.kernelPackages = pkgs.linuxPackages_cachyos;
+        hardware.disk.single.device_id = "/dev/disk/by-id/nvme-CT4000P3PSSD8_2431E8BD13D9";
+
+        impermanence = {
+          enable = true;
+          wipeRootOnBoot = true;
+          wipeHomeOnBoot = true;
+        };
+        system.stateVersion = "25.05";
+      };
+  };
+}
