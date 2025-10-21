e51bf1109b08c60dbf612b3a7d5e7847ee36fadc
Jason Bowman <jason@json64.dev>
2025-10-20 17:15:14 -0700

    update zsh persist files

--- modules/core/firmware/default.nix
+++ modules/core/firmware/default.nix
@@ -2,9 +2,15 @@
   flake.features.firmware.nixos = {
     hardware.enableRedistributableFirmware = true;
     hardware.enableAllFirmware = true;
+
     services.fwupd = {
       enable = true;
     };
+
+    environment.persistence."/persist".files = [
+      "/etc/fwupd/fwupd.conf"
+    ];
+
     environment.persistence."/persist".directories = [
       "/var/cache/fwupd"
       "/var/lib/fwupd"
