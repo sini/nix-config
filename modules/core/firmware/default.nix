b6a65e1e4c3ed4f85427d774b7acbfc7c1d14bc2
Jason Bowman <jason@json64.dev>
2025-10-20 17:30:16 -0700

    no need to persist...

--- modules/core/firmware/default.nix
+++ modules/core/firmware/default.nix
@@ -7,10 +7,6 @@
       enable = true;
     };
 
-    environment.persistence."/persist".files = [
-      "/etc/fwupd/fwupd.conf"
-    ];
-
     environment.persistence."/persist".directories = [
       "/var/cache/fwupd"
       "/var/lib/fwupd"
