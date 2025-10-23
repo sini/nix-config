a6efcbda1b2f4a4fd264d8c68f6e5ba790df7e12
Jason Bowman <jason@json64.dev>
2025-10-23 14:48:51 -0700

    stash zfs progress

--- modules/core/disk/zfs/zfs-disk-single.nix
+++ modules/core/disk/zfs/zfs-disk-single.nix
@@ -124,6 +124,15 @@
               # via config. to achieve this we set options.mountpoint = "legacy" and
               # disable zfs-mount.service
               datasets = {
+                # "reserved" = {
+                #   type = "zfs_fs";
+                #   options = {
+                #     mountpoint = "none";
+                #     canmount = "off";
+                #     reservation = "5GiB";
+                #     "com.sun:auto-snapshot" = "false";
+                #   };
+                # };
                 "local/root" = {
                   mountpoint = "/";
                   type = "zfs_fs";
@@ -170,6 +179,13 @@
                   options."com.sun:auto-snapshot" = "true";
                   postCreateHook = "zfs snapshot zroot/local/volatile@empty";
                 };
+                # "local/containers" = {
+                #   type = "zfs_fs";
+                #   options = {
+                #     mountpoint = "none";
+                #     "com.sun:auto-snapshot" = "false";
+                #   };
+                # };
               };
             };
           };
--- modules/features/podman.nix
+++ modules/features/podman.nix
@@ -1,75 +1,102 @@
 {
-  flake.features.podman.nixos =
-    {
-      config,
-      lib,
-      pkgs,
-      ...
-    }:
-    {
-      environment.systemPackages = with pkgs; [
-        dive # look into docker image layers
-        podman-compose
-        podman-tui # Terminal mgmt UI for Podman
-        passt # For Pasta rootless networking
-        gomanagedocker
-      ];
+  flake.features.podman = {
+    nixos =
+      {
+        config,
+        lib,
+        pkgs,
+        activeFeatures,
+        ...
+      }:
+      let
+        zfsEnabled = builtins.elem "zfs" activeFeatures;
+      in
+      {
+        environment.systemPackages = with pkgs; [
+          dive # look into docker image layers
+          podman-compose
+          podman-tui # Terminal mgmt UI for Podman
+          passt # For Pasta rootless networking
+          gomanagedocker
+        ];
 
-      virtualisation = {
-        containers.enable = true;
-        oci-containers.backend = "podman";
-        podman = {
-          enable = true;
-
-          # prune images and containers periodically
-          autoPrune = {
+        virtualisation = {
+          containers.enable = true;
+          oci-containers.backend = "podman";
+          podman = {
             enable = true;
-            flags = [ "--all" ];
-            dates = "weekly";
-          };
 
-          defaultNetwork.settings =
-            let
-              bridgeNames = lib.sort (a: b: a < b) (builtins.attrNames config.hardware.networking.bridges);
-            in
-            {
-              dns_enabled = true;
-              driver = "bridge";
-              name = builtins.head bridgeNames;
+            extraPackages = [ pkgs.zfs_cachyos ];
+
+            # prune images and containers periodically
+            autoPrune = {
+              enable = true;
+              flags = [ "--all" ];
+              dates = "weekly";
             };
 
-          dockerCompat = true;
-          dockerSocket.enable = true;
-        };
+            defaultNetwork.settings =
+              let
+                bridgeNames = lib.sort (a: b: a < b) (builtins.attrNames config.hardware.networking.bridges);
+              in
+              {
+                dns_enabled = true;
+                driver = "bridge";
+                name = builtins.head bridgeNames;
+              };
 
-        containers.storage.settings = {
-          storage = {
-            driver = "btrfs";
+            dockerCompat = true;
+            dockerSocket.enable = true;
+          };
+
+          containers.storage.settings.storage = {
             runroot = "/run/containers/storage";
             graphroot = "/var/lib/containers/storage";
-            options.overlay.mountopt = "nodev,metacopy=on";
-          }; # storage
+          }
+          // (
+            if zfsEnabled then
+              {
+                driver = "zfs";
+                # options.zfs = {
+                #   fsname = "zroot/local/containers";
+                #   mountopt = "nodev";
+                # };
+              }
+            else
+              {
+                driver = "btrfs";
+                options.overlay.mountopt = "nodev,metacopy=on";
+              }
+          );
         };
+
+        networking.networkmanager.unmanaged = [
+          "interface-name:veth*"
+          "interface-name:podman*"
+          "interface-name:cni*"
+        ];
+
+        # Add 'newuidmap' and 'sh' to the PATH for users' Systemd units.
+        # Required for Rootless podman.
+        systemd.user.extraConfig = ''
+          DefaultEnvironment="PATH=/run/current-system/sw/bin:/run/wrappers/bin:${lib.makeBinPath [ pkgs.bash ]}"
+        '';
+
+        # Allow non-root containers to access lower port numbers
+        boot.kernel.sysctl."net.ipv4.ip_unprivileged_port_start" = 0;
+
+        environment.persistence."/persist".directories = [
+          "/var/lib/cni"
+          "/var/lib/containers"
+        ];
       };
 
-      networking.networkmanager.unmanaged = [
-        "interface-name:veth*"
-        "interface-name:podman*"
-        "interface-name:cni*"
-      ];
-
-      # Add 'newuidmap' and 'sh' to the PATH for users' Systemd units.
-      # Required for Rootless podman.
-      systemd.user.extraConfig = ''
-        DefaultEnvironment="PATH=/run/current-system/sw/bin:/run/wrappers/bin:${lib.makeBinPath [ pkgs.bash ]}"
-      '';
-
-      # Allow non-root containers to access lower port numbers
-      boot.kernel.sysctl."net.ipv4.ip_unprivileged_port_start" = 0;
-
-      environment.persistence."/persist".directories = [
-        "/var/lib/cni"
-        "/var/lib/containers"
-      ];
-    };
+    home =
+      { ... }:
+      {
+        home.persistence."/persist".directories = [
+          ".local/share/containers"
+        ];
+      };
+  };
 }
