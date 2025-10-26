00bd47aba1b635e3920c4f8158bcdb9b7844eba6
Jason Bowman <jason@json64.dev>
2025-10-25 22:52:41 -0700

    feat(impermanence): update gitkraken, razer, polychromatic

--- modules/features/razer.nix
+++ modules/features/razer.nix
@@ -1,14 +1,26 @@
 { config, ... }:
 {
-  flake.features.razer.nixos =
-    { pkgs, ... }:
-    {
-      hardware.openrazer.enable = true;
-      environment.systemPackages = with pkgs; [
-        openrazer-daemon
-        polychromatic
+  flake.features.razer = {
+    nixos =
+      { pkgs, ... }:
+      {
+        hardware.openrazer.enable = true;
+        environment.systemPackages = with pkgs; [
+          openrazer-daemon
+          polychromatic
+        ];
+        hardware.openrazer.users = [ config.flake.meta.user.username ];
+        persistence."/persist".directories = [
+          ".config/openrazer/"
+          ".config/polychromatic/"
+        ];
+      };
+    home = {
+      home.persistence."/persist".directories = [
+        ".config/openrazer/"
+        ".config/polychromatic/"
       ];
-      hardware.openrazer.users = [ config.flake.meta.user.username ];
-    };
 
+    };
+  };
 }
