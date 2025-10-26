7644f98fab71c62fb4ee5f1ab03eabaab1348ca4
Jason Bowman <jason@json64.dev>
2025-10-25 17:42:40 -0700

    chore(treefmt): ignore secret files

--- modules/flake-parts/treefmt.nix
+++ modules/flake-parts/treefmt.nix
@@ -24,6 +24,7 @@
         settings = {
           on-unmatched = "fatal";
           global.excludes = [
+            ".secrets/**"
             "*.editorconfig"
             "*.envrc"
             "*.gitconfig"
