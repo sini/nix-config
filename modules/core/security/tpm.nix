fb99b501caa3a7184601102273bedee4dc3f255b
Jason Bowman <jason@json64.dev>
2025-10-25 17:45:40 -0700

    feat(tpm): enable tpm2 related services

--- modules/core/security/tpm.nix
+++ modules/core/security/tpm.nix
@@ -2,7 +2,12 @@
   flake.features.security.nixos =
     { pkgs, ... }:
     {
-      security.tpm2.enable = true;
+      security.tpm2 = {
+        enable = true;
+        abrmd.enable = true;
+        pkcs11.enable = true;
+        tctiEnvironment.enable = true;
+      };
 
       environment.systemPackages = [
         pkgs.clevis
