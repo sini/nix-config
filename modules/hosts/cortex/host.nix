75db407d4d85b12f48b3865308ee68e4d1b0f317
Jason Bowman <jason@json64.dev>
2025-10-28 12:22:12 -0700

    chore(cortex): Prepare system for re-imaging to ZFS

--- modules/hosts/cortex/host.nix
+++ modules/hosts/cortex/host.nix
@@ -15,11 +15,13 @@
     features = [
       "cpu-amd"
       "gpu-amd"
-      "gpu-nvidia"
-      "gpu-nvidia-prime"
+      # TODO: cuda and rocm don't play well together, create a new microvm for cuda
+      #"gpu-nvidia"
+      #"gpu-nvidia-prime"
       "gpu-nvidia-vfio"
-      "disk-single"
+      "zfs-disk-single"
       "performance"
+      "network-manager"
     ];
     users = {
       "sini" = {
@@ -35,75 +37,32 @@
         ...
       }:
       {
-        # Enable NetworkManager for managing network interfaces
-        networking.networkmanager.enable = true;
-        hardware.disk.single.device_id = "nvme-Samsung_SSD_990_PRO_4TB_S7KGNU0X704630A";
+        hardware.disk.zfs-disk-single.device_id = "/dev/disk/by-id/nvme-Samsung_SSD_990_PRO_4TB_S7KGNU0X704630A";
         hardware.networking.interfaces = [ "enp8s0" ];
-        # hardware.networking.unmanagedInterfaces = [
-        #   "enp8s0"
-        #   "br0"
-        # ];
+        # For Network Manager TODO: RENAME
+        hardware.networking.unmanagedInterfaces = [
+          "enp8s0"
+          "br0"
+        ];
 
         boot.kernelPackages = pkgs.linuxPackages_cachyos.cachyOverride { mArch = "ZEN4"; };
+
         chaotic.hdr = {
           enable = true;
           specialisation.enable = false;
         };
-        # use TCP BBR has significantly increased throughput and reduced latency for connections
-        boot.kernelModules = [
-          "ntsync"
-          "it87" # Fan options
-        ];
+
         # Enable fan sensors...
-        boot.kernelParams = [
-          # Memory Management
-          "default_hugepagesz=2M" # Set default huge page size to 2MB
-          "hugepagesz=2M" # Configure huge page size as 2MB
-          "transparent_hugepage=never" # Disable transparent huge pages
-          "mem_sleep_default=deep" # Set default sleep mode to deep sleep
+        # boot.kernelModules = [
+        #   "it87" # Fan options
+        # ];
+        # boot.extraModprobeConfig = ''
+        #   options it87 ignore_resource_conflict=1 force_id=0x8628
+        # '';
 
-          # ACPI & Power Management
-          "acpi_osi=Linux" # Set ACPI OS interface to Linux
-          "acpi=force" # Force ACPI
-          "acpi_enforce_resources=lax"
-
-          # Performance & Security
-          "mitigations=off" # Disable CPU vulnerabilities mitigations (security trade-off)
-          "nowatchdog" # Disable watchdog timer
-          "nmi_watchdog=0" # Disable NMI watchdog
-          "split_lock_detect=off" # Disable split lock detection
-          "pcie_aspm=off" # Disable PCIe Active State Power Management
-        ];
-
-        boot.extraModprobeConfig = ''
-          options it87 ignore_resource_conflict=1 force_id=0x8628
-        '';
-
-        boot.kernel.sysctl = {
-          "net.core.default_qdisc" = "fq";
-          "net.ipv4.tcp_congestion_control" = "bbr";
-        };
-
-        # networking = {
-        #   interfaces.br0 = {
-        #     useDHCP = true;
-        #   };
-        #   bridges.br0 = {
-        #     interfaces = [ "enp8s0" ];
-        #     rstp = true;
-        #   };
-        #   firewall = {
-        #     allowedUDPPorts = [
-        #       53
-        #       67
-        #     ];
-        #     checkReversePath = false;
-        #   };
-        # };
-
-        environment.systemPackages = with pkgs; [
-          lm_sensors
-        ];
+        # environment.systemPackages = with pkgs; [
+        #   lm_sensors
+        # ];
 
         # Host-specific home-manager configuration
         home-manager.sharedModules = [
@@ -117,10 +76,11 @@
         ];
 
         # impermanence = {
-        #   #enable = true;
-        #   # wipeRootOnBoot = true;
-        #   # wipeHomeOnBoot = true;
+        #   enable = true;
+        #   wipeRootOnBoot = true;
+        #   wipeHomeOnBoot = true;
         # };
+
         system.stateVersion = "25.05";
       };
   };
