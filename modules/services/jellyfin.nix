bbdb59e5a5209cc77731dc51928d3a88fc87ce34
Jason Bowman <jason@json64.dev>
2025-11-03 12:25:54 -0800

    feat(jellyfin): update nginx proxy config

--- modules/services/jellyfin.nix
+++ modules/services/jellyfin.nix
@@ -178,18 +178,9 @@
               proxyPass = "http://127.0.0.1:8096";
               proxyWebsockets = true;
               extraConfig = ''
-                proxy_connect_timeout 5s;
-                proxy_read_timeout 3600s;
-                proxy_send_timeout 3600s;
+                client_max_body_size 20M;
 
-                proxy_set_header Host $host;
-                proxy_set_header X-Real-IP $remote_addr;
-                proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-                proxy_set_header X-Forwarded-Proto $scheme;
-                proxy_set_header X-Forwarded-Protocol $scheme;
-                proxy_set_header X-Forwarded-Host $http_host;
-
-                client_max_body_size 256m;
+                # Disable buffering when the nginx proxy gets very resource heavy upon streaming
                 proxy_buffering off;
               '';
             };
