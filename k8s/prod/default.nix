f8af56c936f141725d4d690ed04998143fc7f7f5
Jason Bowman <jason@json64.dev>
2025-11-06 14:39:18 -0800

    feat(k3s): enable axon-02,axon-03

--- k8s/prod/default.nix
+++ k8s/prod/default.nix
@@ -1,7 +1,15 @@
+# Bootstrap:
+# helm repo add cilium https://helm.cilium.io/
+# helm repo add argo https://argoproj.github.io/argo-helm
+# helm repo update
+# helm upgrade --install cilium cilium/cilium --version 1.18.3 --namespace kube-system --values ciliium/values.yaml
+# kubectl create namespace argocd
+# kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+# nixidy bootstrap .#prod | kubectl apply -f -
 { lib, ... }:
 {
   imports = [
-    # ./argocd.nix
+    ./argocd.nix
 
     # ./caching.nix
     # ./certificates.nix
