194b12862c1f3f4cbc8822e805ed01792037d0e2
Jason Bowman <jason@json64.dev>
2025-11-19 15:17:13 -0800

    feat(hyprland): re-enable hyprland on cortex

--- modules/apps/login/regreet.nix
+++ modules/apps/login/regreet.nix
@@ -1,53 +1,56 @@
 {
-  flake.features.regreet.nixos = {
-    services.greetd.enable = true;
+  flake.features.regreet = {
+    excludes = [ "gdm" ];
+    nixos = {
+      services.greetd.enable = true;
 
-    users.extraUsers.greeter = {
-      home = "/tmp/greeter-home";
-      createHome = true;
-    };
+      users.extraUsers.greeter = {
+        home = "/tmp/greeter-home";
+        createHome = true;
+      };
 
-    # unlock keyring on login
-    security.pam.services.greetd.enableGnomeKeyring = true;
+      # unlock keyring on login
+      security.pam.services.greetd.enableGnomeKeyring = true;
 
-    systemd.services.greetd.serviceConfig = {
-      Type = "idle";
-      StandardInput = "tty";
-      StandardOutput = "tty";
-      StandardError = "journal"; # Without this errors will spam on screen
-      # Without these bootlogs will spam on screen
-      TTYReset = true;
-      TTYVHangup = true;
-      TTYVTDisallocate = true;
-    };
+      systemd.services.greetd.serviceConfig = {
+        Type = "idle";
+        StandardInput = "tty";
+        StandardOutput = "tty";
+        StandardError = "journal"; # Without this errors will spam on screen
+        # Without these bootlogs will spam on screen
+        TTYReset = true;
+        TTYVHangup = true;
+        TTYVTDisallocate = true;
+      };
 
-    programs.regreet = {
-      enable = true;
-      cageArgs = [
-        "-s"
-        "-m"
-        "last"
-      ];
-      settings = {
-        appearance = {
-          greeting_msg = "Hello Word!";
-        };
-        commands = {
-          reboot = [
-            "systemctl"
-            "reboot"
-          ];
-          poweroff = [
-            "systemctl"
-            "poweroff"
-          ];
-        };
-        widget.clock = {
-          format = "%a %T";
-          label_width = 150;
+      programs.regreet = {
+        enable = true;
+        cageArgs = [
+          "-s"
+          "-m"
+          "last"
+        ];
+        settings = {
+          appearance = {
+            greeting_msg = "Hello Word!";
+          };
+          commands = {
+            reboot = [
+              "systemctl"
+              "reboot"
+            ];
+            poweroff = [
+              "systemctl"
+              "poweroff"
+            ];
+          };
+          widget.clock = {
+            format = "%a %T";
+            label_width = 150;
+          };
         };
       };
-    };
 
+    };
   };
 }
