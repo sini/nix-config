339af1df766cd9b1eb7a90da3a4c10b7f8aa98e4
Jason Bowman <jason@json64.dev>
2025-11-20 10:17:51 -0800

    feat(xdg): fix home-manager portal stuff

--- modules/apps/desktop/common/xdg-portal.nix
+++ modules/apps/desktop/common/xdg-portal.nix
@@ -5,23 +5,6 @@
       ...
     }:
     {
-
-      # environment.systemPackages = with pkgs; [
-      #   xdg-launch
-      #   xdg-utils # A set of command line tools that assist apps with a variety of desktop integration tasks
-      #   xdg-user-dirs # Tool to help manage well known user directories like the desktop folder and the music folder
-      #   xdg-dbus-proxy # DBus proxy for Flatpak and others
-      #   xdg-desktop-portal # Desktop integration portals for sandboxed apps
-      #   xdg-desktop-portal-gnome
-      #   xdg-desktop-portal-gtk # Desktop integration portals for sandboxed apps
-      #   #xdg-desktop-portal-hyprland
-      #   inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland
-      #   desktop-file-utils
-      #   libxdg_basedir # Implementation of the XDG Base Directory specification
-      #   shared-mime-info # Database of common MIME types
-      #   mime-types
-      # ];
-
       xdg.portal = {
         enable = true;
         xdgOpenUsePortal = true;
@@ -46,5 +29,15 @@
           # xdg-desktop-portal-hyprland
         ];
       };
+
+      # Necessary for xdg-portal home-manager module to work with useUserPackages enabled
+      # https://github.com/nix-community/home-manager/pull/5184
+      # TODO: When https://github.com/nix-community/home-manager/pull/6981 gets
+      # merged this may no longer be needed
+      environment.pathsToLink = [
+        "/share/xdg-desktop-portal"
+        "/share/applications"
+      ];
+
     };
 }
