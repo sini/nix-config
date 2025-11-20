f6af2b44a8e5e11470030ea32555db6e31e0b89d
Jason Bowman <jason@json64.dev>
2025-11-20 10:21:54 -0800

    feat(uwsm): extract uwsm to seperate module

--- modules/apps/desktop/hyprland/hyprland.nix
+++ modules/apps/desktop/hyprland/hyprland.nix
@@ -1,75 +1,64 @@
 {
-  flake.features.hyprland.nixos =
-    {
-      inputs,
-      pkgs,
-      ...
-    }:
-    let
-      pkgs-unstable = inputs.hyprland.inputs.nixpkgs.legacyPackages.${pkgs.stdenv.hostPlatform.system};
-    in
-    {
-      # Enable cachix
-      nix.settings = {
-        substituters = [ "https://hyprland.cachix.org" ];
-        trusted-substituters = [ "https://hyprland.cachix.org" ];
-        trusted-public-keys = [
-          "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-        ];
-      };
-
-      environment.sessionVariables = {
-        NIXOS_OZONE_WL = "1"; # wayland for electron apps
-        # NOTE: https://github.com/NixOS/nixpkgs/issues/353990
-        GSK_RENDERER = "cairo";
-      };
-
-      # TODO: Mesa broke on actual unstable, pin to hyprland's upstream and wait for it to be fixed
-      hardware.graphics = {
-        package = pkgs-unstable.mesa;
-
-        # if you also want 32-bit support (e.g for Steam)
-        enable32Bit = true;
-        package32 = pkgs-unstable.pkgsi686Linux.mesa;
-      };
-
-      programs.hyprland = {
-        enable = true;
-        package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
-        portalPackage =
-          inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
-        withUWSM = true; # recommended for most users
-        xwayland.enable = true; # Xwayland can be disabled.
-      };
-
-      programs.uwsm.enable = true;
-
-      security.pam.services.gdm.enableGnomeKeyring = true;
-      #services.hypridle.enable = true;
-      #programs.hyprlock.enable = true;
-
-      # programs.gnupg.agent.pinentryPackage = pkgs.writeShellApplication {
-      #   name = "pinentry-rofi";
-      #   runtimeInputs = with pkgs; [ rofi-wayland ];
-      #   text = ''
-      #     exec ${pinentry-rofi}/bin/pinentry-rofi "$@"
-      #   '';
-      # };
-
-      services = {
-        dbus = {
-          implementation = "broker";
-          packages = with pkgs; [
-            gcr
-            gnome-settings-daemon
+  flake.features.hyprland = {
+    requires = [ "uwsm" ];
+    nixos =
+      {
+        inputs,
+        pkgs,
+        ...
+      }:
+      {
+        # Enable cachix
+        nix.settings = {
+          substituters = [ "https://hyprland.cachix.org" ];
+          trusted-substituters = [ "https://hyprland.cachix.org" ];
+          trusted-public-keys = [
+            "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
           ];
         };
 
-        gnome.gnome-keyring.enable = true;
-        gnome.sushi.enable = true;
-        devmon.enable = true;
-        gvfs.enable = true;
-        udisks2.enable = true;
+        environment.sessionVariables = {
+          NIXOS_OZONE_WL = "1"; # wayland for electron apps
+          # NOTE: https://github.com/NixOS/nixpkgs/issues/353990
+          GSK_RENDERER = "cairo";
+        };
+
+        programs.hyprland = {
+          enable = true;
+          package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
+          portalPackage =
+            inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
+          withUWSM = true; # recommended for most users
+          xwayland.enable = true; # Xwayland can be disabled.
+        };
+
+        security.pam.services.gdm.enableGnomeKeyring = true;
+        #services.hypridle.enable = true;
+        #programs.hyprlock.enable = true;
+
+        # programs.gnupg.agent.pinentryPackage = pkgs.writeShellApplication {
+        #   name = "pinentry-rofi";
+        #   runtimeInputs = with pkgs; [ rofi-wayland ];
+        #   text = ''
+        #     exec ${pinentry-rofi}/bin/pinentry-rofi "$@"
+        #   '';
+        # };
+
+        services = {
+          dbus = {
+            implementation = "broker";
+            packages = with pkgs; [
+              gcr
+              gnome-settings-daemon
+            ];
+          };
+
+          gnome.gnome-keyring.enable = true;
+          gnome.sushi.enable = true;
+          devmon.enable = true;
+          gvfs.enable = true;
+          udisks2.enable = true;
+        };
       };
-    };
+  };
 }
