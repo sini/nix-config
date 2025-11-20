3e7e0eea0096c2b219a77b8bdf121be71c928f14
Jason Bowman <jason@json64.dev>
2025-11-20 13:25:50 -0800

    fix: easyeffects home-manager module hangs on startup

--- modules/apps/media/easyeffects/easyeffects.nix
+++ modules/apps/media/easyeffects/easyeffects.nix
@@ -8,10 +8,65 @@
     home =
       { lib, pkgs, ... }:
       {
-        services.easyeffects.enable = true;
+        home.packages = [
+          pkgs.at-spi2-core
+          pkgs.easyeffects
+        ];
 
+        # services.easyeffects.enable = true;
+
+        # systemd.user.services.easyeffects = {
+        #   Service.ExecStart = lib.mkForce "${pkgs.easyeffects}/bin/easyeffects --service-mode --hide-window";
+        # };
+        # systemd.user.services.easyeffects = {
+        #   Unit = {
+        #     Description = "Easyeffects daemon";
+        #     Requires = [ "dbus.service" ];
+        #     After = [ "graphical-session-pre.target" ];
+        #     ConditionEnvironment = "WAYLAND_DISPLAY";
+        #     PartOf = [
+        #       "graphical-session.target"
+        #       "pipewire.service"
+        #     ];
+        #   };
+
+        #   Install.WantedBy = [ "graphical-session.target" ];
+
+        #   Service = {
+        #     ExecStart = "${pkgs.easyeffects}/bin/easyeffects --service-mode --hide-window";
+        #     ExecStop = "${pkgs.easyeffects}/bin/easyeffects --quit";
+        #     Restart = "on-failure";
+        #     RestartSec = 5;
+        #   };
+        # };
         systemd.user.services.easyeffects = {
-          Service.ExecStart = lib.mkForce "${pkgs.easyeffects}/bin/easyeffects --service-mode --hide-window";
+          Unit = {
+            After = lib.mkForce [
+              "pipewire.service"
+              "graphical-session.target"
+            ];
+            BindsTo = lib.mkForce [
+              "pipewire.service"
+              "graphical-session.target"
+            ];
+            PartOf = lib.mkForce [ ];
+          };
+
+          Install.WantedBy = lib.mkForce [ "pipewire.service" ];
+
+          Service = {
+            Slice = "background.slice";
+            Type = "simple";
+            ExecStart = "${pkgs.easyeffects}/bin/easyeffects --service-mode --hide-window";
+            ExecStop = "${pkgs.easyeffects}/bin/easyeffects --quit";
+            Restart = "on-failure";
+            KillMode = "mixed";
+            RestartSec = 5;
+            TimeoutStopSec = 10;
+            # ExecStartPost = [
+            #   "${lib.getExe config.services.easyeffects.package} --load-preset ${config.services.easyeffects.preset}"
+            # ];
+          };
         };
 
         xdg.configFile = {
--- modules/apps/productivity/audio.nix
+++
@@ -1,6 +0,0 @@
-{
-  flake.features.audio.home = {
-    # Enable easyeffects audio processing
-    services.easyeffects.enable = true;
-  };
-}
