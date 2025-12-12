48bc97bf776d6e0e58ec7586952e69f2d409c120
Jason Bowman <jason@json64.dev>
2025-12-12 12:22:38 -0800

    chore: remove override now that its fixed upstream

--- modules/apps/productivity/firefox.nix
+++ modules/apps/productivity/firefox.nix
@@ -4,10 +4,6 @@
     {
       programs.firefox = {
         enable = true;
-        # TODO Firefox fails as the closure contains a reference to stdenv.cc
-        # Relax this assertion until the underlying issue is fixed
-        # https://github.com/NixOS/nixpkgs/pull/457424
-        package = pkgs.firefox.overrideAttrs { disallowedRequisites = [ ]; };
 
         # nativeMessagingHosts = [
         #   pkgs.ff2mpv
