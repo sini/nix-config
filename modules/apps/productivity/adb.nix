4275fb8782d3f81087500415af343bfac802f6ac
Jason Bowman <jason@json64.dev>
2026-01-05 10:44:05 -0800

    chore: re-enable features

--- modules/apps/productivity/adb.nix
+++ modules/apps/productivity/adb.nix
@@ -6,9 +6,9 @@
       users.users.${config.flake.meta.user.username}.extraGroups = [
         "adbusers"
       ];
-      programs.adb.enable = true;
 
       environment.systemPackages = [
+        pkgs.android-tools
         pkgs.android-file-transfer # => <https://github.com/whoozle/android-file-transfer-linux>
       ];
     };
