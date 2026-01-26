42f301a821a4f1c62f906bf1eea50abac3fdb95e
Jason Bowman <jason@json64.dev>
2026-01-26 13:27:33 -0800

    feat(misc-tools): enable discordo, wiremix, impala, bluetui

--- modules/core/network/wireless.nix
+++ modules/core/network/wireless.nix
@@ -1,48 +1,58 @@
 { rootPath, ... }:
 {
-  flake.features.wireless.nixos =
-    {
-      config,
-      environment,
-      ...
-    }:
-    {
-      # # This is just a wpa_supplicant.conf for booting, it's super simple -- one ESSID + psk
-      # age.secrets.wpa-supplicant-config = {
-      #   rekeyFile = rootPath + "/.secrets/user/wpa_supplicant-config.age";
-      #   path = "/root/.wpa_supplicant/wpa_supplicant.conf";
-      #   symlink = false;
-      # };
+  flake.features.wireless = {
+    nixos =
+      {
+        config,
+        environment,
+        ...
+      }:
+      {
+        # # This is just a wpa_supplicant.conf for booting, it's super simple -- one ESSID + psk
+        # age.secrets.wpa-supplicant-config = {
+        #   rekeyFile = rootPath + "/.secrets/user/wpa_supplicant-config.age";
+        #   path = "/root/.wpa_supplicant/wpa_supplicant.conf";
+        #   symlink = false;
+        # };
 
-      age.secrets.wpa-supplicant = {
-        rekeyFile = rootPath + "/.secrets/env/${environment.name}/wpa_supplicant-arcade.age";
-      };
+        age.secrets.wpa-supplicant = {
+          rekeyFile = rootPath + "/.secrets/env/${environment.name}/wpa_supplicant-arcade.age";
+        };
 
-      environment.persistence."/cache".directories = [
-        "/etc/wpa_supplicant"
-        "/var/lib/iwd"
-      ];
-
-      # Ensure a file exists so that we can write the initrd, even if it's not valid
-      system.activationScripts.agenixEnsureInitrdWpaSupplicantConfig = {
-        text = ''
-          [[ -e ${config.age.secrets.wpa-supplicant.path} ]] \
-            || touch ${config.age.secrets.wpa-supplicant.path}
-        '';
-        deps = [
-          "agenixInstall"
-          "users"
+        environment.persistence."/cache".directories = [
+          "/etc/wpa_supplicant"
+          "/var/lib/iwd"
         ];
-      };
 
-      system.activationScripts.agenixChown.deps = [ "agenixEnsureInitrdWpaSupplicantConfig" ];
+        # Ensure a file exists so that we can write the initrd, even if it's not valid
+        system.activationScripts.agenixEnsureInitrdWpaSupplicantConfig = {
+          text = ''
+            [[ -e ${config.age.secrets.wpa-supplicant.path} ]] \
+              || touch ${config.age.secrets.wpa-supplicant.path}
+          '';
+          deps = [
+            "agenixInstall"
+            "users"
+          ];
+        };
 
-      networking.wireless = {
-        enable = true;
-        secretsFile = config.age.secrets.wpa-supplicant.path;
-        networks = {
-          "The Arcade".pskRaw = "ext:psk_arcade";
+        system.activationScripts.agenixChown.deps = [ "agenixEnsureInitrdWpaSupplicantConfig" ];
+
+        networking.wireless = {
+          enable = true;
+          secretsFile = config.age.secrets.wpa-supplicant.path;
+          networks = {
+            "The Arcade".pskRaw = "ext:psk_arcade";
+          };
         };
       };
-    };
+
+    home =
+      { pkgs, ... }:
+      {
+        home.packages = [
+          pkgs.impala
+        ];
+      };
+  };
 }
--- modules/hardware/bluetooth.nix
+++ modules/hardware/bluetooth.nix
@@ -1,48 +1,42 @@
 {
-  flake.features.bluetooth.nixos =
-    { pkgs, ... }:
-    {
-      # environment.persistence."/persist/system" = lib.mkIf isImpermanent {
-      #   directories = [
-      #     "/var/lib/bluetooth"
-      #   ];
-      # };
-
-      hardware.bluetooth = {
-        enable = true;
-        package = pkgs.bluez-experimental;
-        powerOnBoot = true;
-        disabledPlugins = [ "sap" ];
-        settings = {
-          Policy.AutoEnable = true;
-          General = {
-            Privacy = "device";
-            FastConnectable = true;
-            Experimental = true;
-            KernelExperimental = true;
-            JustWorksRepairing = "always";
-            MultiProfile = "multiple";
-            Class = "0x000100"; # Computer class
-            # Class = 0x100100 # (Object-Transfer Service & Computer Type)
-            Enable = "Source,Sink,Media,Socket";
+  flake.features.bluetooth = {
+    nixos =
+      { pkgs, ... }:
+      {
+        hardware.bluetooth = {
+          enable = true;
+          package = pkgs.bluez-experimental;
+          powerOnBoot = true;
+          disabledPlugins = [ "sap" ];
+          settings = {
+            Policy.AutoEnable = true;
+            General = {
+              Privacy = "device";
+              FastConnectable = true;
+              Experimental = true;
+              KernelExperimental = true;
+              JustWorksRepairing = "always";
+              MultiProfile = "multiple";
+              Class = "0x000100"; # Computer class
+              # Class = 0x100100 # (Object-Transfer Service & Computer Type)
+              Enable = "Source,Sink,Media,Socket";
+            };
           };
         };
+
+        boot.kernelParams = [ "btusb" ];
+
+        services.blueman.enable = true;
+
+        environment.persistence."/cache".directories = [ "/var/lib/bluetooth" ];
       };
 
-      boot.kernelParams = [ "btusb" ];
-
-      services.blueman.enable = true;
-
-      # services.pulseaudio = {
-      #   package = pkgs.pulseaudio.override { bluetoothSupport = true; };
-      #   extraConfig = ''
-      #     load-module module-bluetooth-discover
-      #     load-module module-bluetooth-policy
-      #     load-module module-switch-on-connect
-      #   '';
-      #   extraModules = with pkgs; [ pulseaudio-modules-bt ];
-      # };
-
-      environment.persistence."/cache".directories = [ "/var/lib/bluetooth" ];
-    };
+    home =
+      { pkgs, ... }:
+      {
+        home.packages = [
+          pkgs.bluetui
+        ];
+      };
+  };
 }
