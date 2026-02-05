12f77bd49e769f5b5fcc7fb9f5fb5e207fcf266d
Jason Bowman <jason@json64.dev>
2026-02-05 15:08:59 -0800

    feat(nixidy): stash progress

--- modules/kubernetes/services/argocd/argocd.nix
+++ modules/kubernetes/services/argocd/argocd.nix
@@ -36,7 +36,7 @@
 
               # Redis (for caching)
               redis = {
-                enabled = true;
+                enabled = false;
               };
 
               # Disable HA redis for local dev
@@ -79,6 +79,100 @@
                 '';
               };
               global.networkPolicy.create = true;
+            };
+          };
+
+          resources = {
+            # Allow ingress traffic from traefik to
+            # argocd-server.
+            # networkPolicies.allow-traefik-ingress.spec = {
+            #   podSelector.matchLabels."app.kubernetes.io/name" = "argocd-server";
+            #   policyTypes = [ "Ingress" ];
+            #   ingress = [
+            #     {
+            #       from = [
+            #         {
+            #           namespaceSelector.matchLabels."kubernetes.io/metadata.name" = "traefik";
+            #           podSelector.matchLabels."app.kubernetes.io/name" = "traefik";
+            #         }
+            #       ];
+            #       ports = [
+            #         {
+            #           protocol = "TCP";
+            #           port = 8080;
+            #         }
+            #       ];
+            #     }
+            #   ];
+            # };
+
+            ciliumNetworkPolicies = {
+              # Allow argocd-repo-server egress access to github.com
+              allow-world-egress.spec = {
+                endpointSelector.matchLabels."app.kubernetes.io/name" = "argocd-repo-server";
+                egress = [
+                  # Enable DNS proxying
+                  {
+                    toEndpoints = [
+                      {
+                        matchLabels = {
+                          "k8s:io.kubernetes.pod.namespace" = "kube-system";
+                          "k8s:k8s-app" = "kube-dns";
+                        };
+                      }
+                    ];
+                    toPorts = [
+                      {
+                        ports = [
+                          {
+                            port = "53";
+                            protocol = "ANY";
+                          }
+                        ];
+                        rules.dns = [
+                          { matchPattern = "*"; }
+                        ];
+                      }
+                    ];
+                  }
+                  # Allow HTTPS to github.com
+                  {
+                    toFQDNs = [
+                      { matchName = "github.com"; }
+                    ];
+                    toPorts = [
+                      {
+                        ports = [
+                          {
+                            port = "443";
+                            protocol = "TCP";
+                          }
+                        ];
+                      }
+                    ];
+                  }
+                ];
+              };
+
+              # Allow all ArgoCD pods to access kube-apiserver
+              allow-kube-apiserver-egress.spec = {
+                endpointSelector.matchLabels."app.kubernetes.io/part-of" = "argocd";
+                egress = [
+                  {
+                    toEntities = [ "kube-apiserver" ];
+                    toPorts = [
+                      {
+                        ports = [
+                          {
+                            port = "6443";
+                            protocol = "TCP";
+                          }
+                        ];
+                      }
+                    ];
+                  }
+                ];
+              };
             };
           };
 
