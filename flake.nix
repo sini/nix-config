8ed566fa45b44cafc0273da6a7127fd772b3b3f2
Jason Bowman <jason@json64.dev>
2026-02-11 13:39:09 -0800

    fix: temporarily point razerdaemon at my fork with fixes

--- flake.nix
+++ flake.nix
@@ -227,7 +227,10 @@
       inputs.nixpkgs.follows = "nixpkgs-unstable";
     };
 
-    razerdaemon.url = "github:encomjp/razer-control-revived";
+    razerdaemon = {
+      url = "github:sini/razer-control-revived";
+      inputs.nixpkgs.follows = "nixpkgs-unstable";
+    };
 
     # global, so they can be `.follow`ed
     systems.url = "github:nix-systems/default";
