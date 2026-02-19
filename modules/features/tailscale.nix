ef1805598094bfb2876f3b00d2b9228d2e759295
Jason Bowman <jason@json64.dev>
2026-02-18 18:59:03 -0800

    feat(tailscale): disable nftables

--- modules/features/tailscale.nix
+++ modules/features/tailscale.nix
@@ -1,16 +1,7 @@
 { rootPath, ... }:
 {
   flake.features.tailscale.nixos =
-    {
-      config,
-      environment,
-      # lib,
-      # activeFeatures,
-      ...
-    }:
-    # let
-    # isMobile = lib.elem "laptop" activeFeatures;
-    # in
+    { config, environment, ... }:
     {
       # sudo headscale preauthkeys create --user 1 --reusable -e 10y
       age.secrets.tailscale-auth-key = {
@@ -22,24 +13,15 @@
         openFirewall = true;
         authKeyFile = config.age.secrets.tailscale-auth-key.path;
         extraUpFlags = [ "--login-server=https://hs.${environment.domain}" ];
-        # extraUpFlags = lib.mkIf (!isMobile) [
-        #   "--advertise-exit-node"
-        #   "--exit-node-allow-lan-access"
-        # ];
         extraDaemonFlags = [ "--no-logs-no-support" ];
       };
 
       networking = {
-        nftables.enable = true;
         firewall = {
           trustedInterfaces = [ config.services.tailscale.interfaceName ];
           allowedUDPPorts = [ config.services.tailscale.port ];
         };
       };
-
-      systemd.services.tailscaled.serviceConfig.Environment = [
-        "TS_DEBUG_FIREWALL_MODE=nftables"
-      ];
 
       environment.persistence."/persist".directories = [
         "/var/lib/tailscale"
