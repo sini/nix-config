ec47802cb1f00117abdebc97633d94fa3d059ae8
Jason Bowman <jason@json64.dev>
2026-02-20 10:15:41 -0800

    feat(nix-flake-build): support passing arguments

--- pkgs/by-name/nix-flake-build/package.nix
+++ pkgs/by-name/nix-flake-build/package.nix
@@ -9,11 +9,27 @@ writeShellApplication {
   runtimeInputs = [ nix-output-monitor ];
   text = ''
     [[ "$#" -ge 1 ]] \
-      || { echo "usage: nix-flake-build <HOST>..." >&2; exit 1; }
+      || { echo "usage: nix-flake-build [OPTIONS...] <HOST>..." >&2; exit 1; }
+
+    OPTIONS=()
+    HOST_NAMES=()
+
+    for arg in "$@"; do
+      if [[ "$arg" == -* ]]; then
+        OPTIONS+=("$arg")
+      else
+        HOST_NAMES+=("$arg")
+      fi
+    done
+
+    [[ "''${#HOST_NAMES[@]}" -ge 1 ]] \
+      || { echo "error: at least one HOST must be specified" >&2; exit 1; }
+
     HOSTS=()
-    for h in "$@"; do
+    for h in "''${HOST_NAMES[@]}"; do
       HOSTS+=(".#nixosConfigurations.$h.config.system.build.toplevel")
     done
-    nom build --no-link --print-out-paths --show-trace "''${HOSTS[@]}"
+
+    nom build --no-link --print-out-paths --show-trace "''${OPTIONS[@]}" "''${HOSTS[@]}"
   '';
 }
