02ddf62796b0239a0961a997bb4607d914b012a5
Jason Bowman <jason@json64.dev>
2026-02-19 16:36:31 -0800

    feat(cilium): hubble cert -> self gen

--- modules/kubernetes/services/network/cilium/cilium.nix
+++ modules/kubernetes/services/network/cilium/cilium.nix
@@ -124,13 +124,14 @@ in
                 tls = {
                   auto = {
                     enabled = true;
-                    method = "certmanager";
-                    certValidityDuration = 90;
-                    certManagerIssuerRef = {
-                      group = "cert-manager.io";
-                      kind = "ClusterIssuer";
-                      name = "cloudflare-issuer";
-                    };
+                    method = "cronJob";
+                    # method = "certmanager";
+                    # certValidityDuration = 90;
+                    # certManagerIssuerRef = {
+                    #   group = "cert-manager.io";
+                    #   kind = "ClusterIssuer";
+                    #   name = "cloudflare-issuer";
+                    # };
                   };
                   server.extraDnsNames = [ "*.mesh.${environment.name}.${environment.domain}" ];
                 };
@@ -387,195 +388,6 @@ in
 
             };
           };
-
-          # resources = {
-          #   ciliumNetworkPolicies = {
-          #     # Allow hubble relay server egress to nodes
-          #     allow-hubble-relay-server-egress.spec = {
-          #       description = "Policy for egress from hubble relay to hubble server in Cilium agent.";
-          #       endpointSelector.matchLabels."app.kubernetes.io/name" = "hubble-relay";
-          #       egress = [
-          #         {
-          #           toEntities = [
-          #             "remote-node"
-          #             "host"
-          #           ];
-          #           toPorts = [
-          #             {
-          #               ports = [
-          #                 {
-          #                   port = "4244";
-          #                   protocol = "TCP";
-          #                 }
-          #               ];
-          #             }
-          #           ];
-          #         }
-          #       ];
-          #     };
-
-          #     # Allow hubble UI to talk to hubble relay
-          #     allow-hubble-ui-relay-ingress.spec = {
-          #       description = "Policy for ingress from hubble UI to hubble relay.";
-          #       endpointSelector.matchLabels."app.kubernetes.io/name" = "hubble-relay";
-          #       ingress = [
-          #         {
-          #           fromEndpoints = [
-          #             {
-          #               matchLabels."app.kubernetes.io/name" = "hubble-ui";
-          #             }
-          #           ];
-          #           toPorts = [
-          #             {
-          #               ports = [
-          #                 {
-          #                   port = "4245";
-          #                   protocol = "TCP";
-          #                 }
-          #               ];
-          #             }
-          #           ];
-          #         }
-          #       ];
-          #     };
-
-          #     # Allow hubble UI to talk to kube-apiserver
-          #     allow-hubble-ui-kube-apiserver-egress.spec = {
-          #       description = "Allow Hubble UI to talk to kube-apiserver";
-          #       endpointSelector.matchLabels."app.kubernetes.io/name" = "hubble-ui";
-          #       egress = [
-          #         {
-          #           toEntities = [ "kube-apiserver" ];
-          #           toPorts = [
-          #             {
-          #               ports = [
-          #                 {
-          #                   port = "6443";
-          #                   protocol = "TCP";
-          #                 }
-          #               ];
-          #             }
-          #           ];
-          #         }
-          #       ];
-          #     };
-
-          #     # Allow kube-dns to talk to upstream DNS
-          #     allow-kube-dns-upstream-egress.spec = {
-          #       description = "Policy for egress to allow kube-dns to talk to upstream DNS.";
-          #       endpointSelector.matchLabels.k8s-app = "kube-dns";
-          #       egress = [
-          #         {
-          #           toEntities = [ "world" ];
-          #           toPorts = [
-          #             {
-          #               ports = [
-          #                 {
-          #                   port = "53";
-          #                   protocol = "UDP";
-          #                 }
-          #               ];
-          #             }
-          #           ];
-          #         }
-          #       ];
-          #     };
-
-          #     # Allow CoreDNS to talk to kube-apiserver
-          #     allow-kube-dns-apiserver-egress.spec = {
-          #       description = "Allow coredns to talk to kube-apiserver.";
-          #       endpointSelector.matchLabels.k8s-app = "kube-dns";
-          #       egress = [
-          #         {
-          #           toEntities = [ "kube-apiserver" ];
-          #           toPorts = [
-          #             {
-          #               ports = [
-          #                 {
-          #                   port = "6443";
-          #                   protocol = "TCP";
-          #                 }
-          #               ];
-          #             }
-          #           ];
-          #         }
-          #       ];
-          #     };
-
-          #     # Allow hubble-generate-certs job to talk to kube-apiserver
-          #     allow-hubble-generate-certs-apiserver-egress.spec = {
-          #       description = "Allow hubble-generate-certs job to talk to kube-apiserver.";
-          #       endpointSelector.matchLabels."batch.kubernetes.io/job-name" = "hubble-generate-certs";
-          #       egress = [
-          #         {
-          #           toEntities = [ "kube-apiserver" ];
-          #           toPorts = [
-          #             {
-          #               ports = [
-          #                 {
-          #                   port = "6443";
-          #                   protocol = "TCP";
-          #                 }
-          #               ];
-          #             }
-          #           ];
-          #         }
-          #       ];
-          #     };
-          #   };
-
-          #   ciliumClusterwideNetworkPolicies = {
-          #     # Allow all cilium endpoints to talk egress to each other
-          #     allow-internal-egress.spec = {
-          #       description = "Policy to allow all Cilium managed endpoint to talk to all other cilium managed endpoints on egress";
-          #       endpointSelector = { };
-          #       egress = [
-          #         {
-          #           toEndpoints = [ { } ];
-          #         }
-          #       ];
-          #     };
-
-          #     # Allow all health checks
-          #     cilium-health-checks.spec = {
-          #       endpointSelector.matchLabels."reserved:health" = "";
-          #       ingress = [
-          #         {
-          #           fromEntities = [ "remote-node" ];
-          #         }
-          #       ];
-          #       egress = [
-          #         {
-          #           toEntities = [ "remote-node" ];
-          #         }
-          #       ];
-          #     };
-
-          #     # Allow all cilium managed endpoints to talk to cluster dns
-          #     allow-kube-dns-cluster-ingress.spec = {
-          #       description = "Policy for ingress allow to kube-dns from all Cilium managed endpoints in the cluster.";
-          #       endpointSelector.matchLabels = {
-          #         "k8s:io.kubernetes.pod.namespace" = "kube-system";
-          #         "k8s-app" = "kube-dns";
-          #       };
-          #       ingress = [
-          #         {
-          #           fromEndpoints = [ { } ];
-          #           toPorts = [
-          #             {
-          #               ports = [
-          #                 {
-          #                   port = "53";
-          #                   protocol = "UDP";
-          #                 }
-          #               ];
-          #             }
-          #           ];
-          #         }
-          #       ];
-          #     };
-          #   };
-          # };
         };
       };
   };
