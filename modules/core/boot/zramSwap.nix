bb2b07f7fed78faf1e1a173adcfd0dd28b499c83
Jason Bowman <jason@json64.dev>
2026-02-21 12:18:27 -0800

    chore: enable earlyoom

--- modules/core/boot/zramSwap.nix
+++ modules/core/boot/zramSwap.nix
@@ -1,5 +1,10 @@
 {
   flake.features.systemd-boot.nixos = {
     zramSwap.enable = true;
+
+    # Ensure a single individual task doesn't freeze the system, without trusting the random action of the kernel
+    # out-of-memory (OOM) killer.
+    services.earlyoom.enable = true;
+    services.earlyoom.freeMemThreshold = 2; # Percentage of total RAM
   };
 }
