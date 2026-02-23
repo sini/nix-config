d49184859a8a9803bbe4b8769f2f4ef83eb8c8e7
Jason Bowman <jason@json64.dev>
2026-02-23 11:39:53 -0800

    feat(cilium): masqerade tuning

--- modules/environments/prod/kubernetes.nix
+++ modules/environments/prod/kubernetes.nix
@@ -34,14 +34,14 @@
       config = {
         cilium = {
           devices = [
-            # "dummy0"
+            "dummy0"
             # "enp199s0f5"
             # "enp199s0f6"
             "br0"
-            # "enp2s0"
+            "enp2s0"
             # "tailscale0"
           ];
-          # directRoutingDevice = "dummy0";
+          directRoutingDevice = "dummy0";
         };
         sops-secrets-operator.replicaCount = 1; # High availability for prod
       };
