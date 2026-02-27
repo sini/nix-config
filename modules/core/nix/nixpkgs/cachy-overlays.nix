66fee49fde6ff026ecdf1493b8ef35be15c3f92b
Jason Bowman <jason@json64.dev>
2026-02-27 11:56:16 -0800

    chore: update flake.lock and cachy kernel pinning

--- modules/core/nix/nixpkgs/cachy-overlays.nix
+++ modules/core/nix/nixpkgs/cachy-overlays.nix
@@ -3,7 +3,7 @@
     { inputs, ... }:
     {
       nixpkgs.overlays = [
-        inputs.nix-cachyos-kernel.overlays.pinned
+        inputs.nix-cachyos-kernel.overlays.default
         inputs.proton-cachyos.overlays.default
       ];
     };
