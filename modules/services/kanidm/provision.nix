6c69782fb160a9ec74e2475c9aac1b074861124e
Jason Bowman <jason@json64.dev>
2026-02-27 13:29:25 -0800

    feat(kanidm): refactor provisioning structure

--- modules/services/kanidm/provision.nix
+++ modules/services/kanidm/provision.nix
@@ -94,121 +94,125 @@
           json = {
             displayName = "Jason";
             mailAddresses = [ "jason@${environment.email.domain}" ];
+            groups = [ "admins" ];
           };
           shuo = {
             displayName = "Shuo";
             mailAddresses = [ "shuo@${environment.email.domain}" ];
+            groups = [ "admins" ];
           };
           will = {
             displayName = "Will";
             mailAddresses = [ "will@${environment.email.domain}" ];
+            groups = [
+              "users"
+              "grafana.server-admins"
+              "open-webui.access"
+            ];
           };
           greco = {
             displayName = "Jason";
             mailAddresses = [ "greco@${environment.email.domain}" ];
+            groups = [ "users" ];
+
           };
           taiche = {
             displayName = "Chris";
             mailAddresses = [ "taiche@${environment.email.domain}" ];
+            groups = [ "users" ];
+
           };
           jennism = {
             displayName = "Jennifer";
             mailAddresses = [ "jennism@${environment.email.domain}" ];
+            groups = [ "users" ];
+
           };
           hugs = {
             displayName = "Shawn";
             mailAddresses = [ "hugs@${environment.email.domain}" ];
+            groups = [
+              "users"
+              "grafana.server-admins"
+            ];
           };
           ellen = {
             displayName = "Ellen";
             mailAddresses = [ "ellen@${environment.email.domain}" ];
+            groups = [ "users" ];
+
           };
           jenn = {
             displayName = "Jennifer";
             mailAddresses = [ "jenn@${environment.email.domain}" ];
+            groups = [ "users" ];
+
           };
           tyr = {
             displayName = "tyr";
             mailAddresses = [ "tyr@${environment.email.domain}" ];
+            groups = [ "users" ];
+
           };
           zogger = {
             displayName = "zogger";
             mailAddresses = [ "zogger@${environment.email.domain}" ];
+            groups = [ "users" ];
           };
           jess = {
             displayName = "jess";
             mailAddresses = [ "jess@${environment.email.domain}" ];
+            groups = [ "users" ];
+
           };
           leo = {
             displayName = "leo";
             mailAddresses = [ "leo@${environment.email.domain}" ];
+            groups = [ "users" ];
           };
           vincentpierre = {
             displayName = "vincentpierre";
             mailAddresses = [ "vincentpierre@${environment.email.domain}" ];
+            groups = [ "users" ];
           };
           you = {
             displayName = "You";
             mailAddresses = [ "you@${environment.email.domain}" ];
+            groups = [ "users" ];
           };
           yiran = {
             displayName = "Yiran";
             mailAddresses = [ "yiran@${environment.email.domain}" ];
+            groups = [ "users" ];
           };
           louisabella = {
             displayName = "louisabella";
             mailAddresses = [ "louisabella@${environment.email.domain}" ];
+            groups = [ "users" ];
           };
         };
 
         # OAuth2 clients and groups for services
         groups = {
-          "admins".members = [
-            "json"
-            "shuo"
-          ];
+          "admins" = { };
 
-          "users" = { };
+          "users".members = [ "admins" ];
 
-          "grafana.access".members = [
-            "json"
-            "shuo"
-            "will"
-            "hugs"
-          ];
+          "grafana.access".members = [ "users" ];
 
           "grafana.editors" = { };
           "grafana.admins" = { };
-          "grafana.server-admins".members = [
-            "json"
-            "shuo"
-            "will"
-            "hugs"
-          ];
+          "grafana.server-admins".members = [ "admins" ];
 
-          "open-webui.access".members = [
-            "json"
-            "shuo"
-            "will"
-            "greco"
-            "hugs"
-          ];
+          "open-webui.access".members = [ "admins" ];
 
-          "open-webui.admins".members = [
-            "admins"
-          ];
+          "open-webui.admins".members = [ "admins" ];
 
-          "media.access".members = [
-            "users"
-          ];
+          "media.access".members = [ "users" ];
 
-          "media.admins".members = [
-            "admins"
-          ];
+          "media.admins".members = [ "admins" ];
 
-          "vpn.users".members = [
-            "admins"
-          ];
+          "vpn.users".members = [ "admins" ];
         };
 
         systems.oauth2 = {
