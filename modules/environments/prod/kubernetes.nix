3439fb545f1555fdcc025610874160e5f0073509
Jason Bowman <jason@json64.dev>
2026-03-04 22:11:05 -0800

    chore: disable axon cluster axon cluster

--- modules/environments/prod/kubernetes.nix
+++ modules/environments/prod/kubernetes.nix
@@ -48,7 +48,7 @@
         # Storage drivers
         "volume-snapshots"
         "csi-driver-nfs"
-        "rook-ceph"
+        #"rook-ceph"
       ];
       config = {
         coredns.clusterIP = "172.21.0.10";
