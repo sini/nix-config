a2ba0fc0c9f77c3f60fe4d54b8f45225e3307043
Jason Bowman <jason@json64.dev>
2026-03-06 18:41:50 -0800

    feat(nixhelm): add local user chart support

---
+++ charts/rocm/amd-gpu/default.nix
@@ -0,0 +1,6 @@
+{
+  repo = "https://rocm.github.io/k8s-device-plugin/";
+  chart = "amd-gpu";
+  version = "0.21.0";
+  chartHash = "sha256-Rc2Fb0774k7pc8b9eDLhsb6QejVTPY4jYZYRkmks9EI=";
+}
---
+++ modules/flake-parts/helm-charts.nix
@@ -0,0 +1,45 @@
+{
+  inputs,
+  config,
+  lib,
+  ...
+}:
+let
+  mkCharts =
+    { pkgs }:
+    let
+      kubelib = inputs.nix-kube-generators.lib { inherit pkgs; };
+      trimBogusVersion = attrs: removeAttrs attrs [ "bogusVersion" ];
+    in
+    inputs.haumea.lib.load {
+      src = ../../charts;
+      loader = { ... }: p: kubelib.downloadHelmChart (trimBogusVersion (import p));
+      transformer = inputs.haumea.lib.transformers.liftDefault;
+    };
+in
+{
+  flake = {
+    chartsMetadata = inputs.haumea.lib.load {
+      src = ../../charts;
+      transformer = inputs.haumea.lib.transformers.liftDefault;
+    };
+
+    charts = mkCharts;
+
+    chartsDerivations = lib.genAttrs config.systems (
+      system: mkCharts { pkgs = inputs.nixpkgs-unstable.legacyPackages.${system}; }
+    );
+  };
+
+  perSystem =
+    { inputs', ... }:
+    {
+      devshells.default.commands = [
+        {
+          name = "helmupdater";
+          command = ''${inputs'.nixhelm.packages.helmupdater}/bin/helmupdater "$@"'';
+          help = "Update helm chart versions and hashes";
+        }
+      ];
+    };
+}
