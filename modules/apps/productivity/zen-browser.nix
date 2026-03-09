601458f0d45a786a216e105f9595be6396abfe2b
Jason Bowman <jason@json64.dev>
2026-03-08 17:49:07 -0700

    feat(cortex): disable zenbrowser

--- modules/apps/productivity/zen-browser.nix
+++ modules/apps/productivity/zen-browser.nix
@@ -11,7 +11,7 @@
       ];
       programs.zen-browser = {
         enable = true;
-        nativeMessagingHosts = [ pkgs.firefoxpwa ];
+        # nativeMessagingHosts = [ pkgs.firefoxpwa ];
 
         # stylix.targets.zen-browser.profileNames = [ "<PROFILE_NAME>" ];
 
