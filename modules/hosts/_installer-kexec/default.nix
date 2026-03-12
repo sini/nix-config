66d3c638956369fb46972714d351bca6da1bbc81
Jason Bowman <jason@json64.dev>
2026-03-12 12:41:54 -0700

    fix: linting error

--- modules/hosts/_installer-kexec/default.nix
+++ modules/hosts/_installer-kexec/default.nix
@@ -1,4 +1,10 @@
-{ config, lib, modulesPath, pkgs, ... }:
+{
+  config,
+  lib,
+  modulesPath,
+  pkgs,
+  ...
+}:
 let
   # Static binaries for kexec
   iprouteStatic = pkgs.pkgsStatic.iproute2.override { iptables = null; };
@@ -45,33 +51,34 @@ in
 
   # Use xz compression for faster boot
   boot.initrd.compressor = "xz";
+  system = {
+    # Build the kexec run script
+    build.kexecRun = pkgs.runCommand "kexec-run" { } ''
+      install -D -m 0755 ${./kexec-run.sh} $out
 
-  # Build the kexec run script
-  system.build.kexecRun = pkgs.runCommand "kexec-run" { } ''
-    install -D -m 0755 ${./kexec-run.sh} $out
+      sed -i \
+        -e 's|@init@|${config.system.build.toplevel}/init|' \
+        -e 's|@kernelParams@|${lib.escapeShellArgs config.boot.kernelParams}|' \
+        $out
 
-    sed -i \
-      -e 's|@init@|${config.system.build.toplevel}/init|' \
-      -e 's|@kernelParams@|${lib.escapeShellArgs config.boot.kernelParams}|' \
-      $out
+      ${pkgs.shellcheck}/bin/shellcheck $out
+    '';
 
-    ${pkgs.shellcheck}/bin/shellcheck $out
-  '';
+    # Build the kexec installer tarball
+    build.kexecInstallerTarball = pkgs.runCommand "kexec-tarball" { } ''
+      mkdir kexec $out
+      cp "${config.system.build.netbootRamdisk}/initrd" kexec/initrd
+      cp "${config.system.build.kernel}/${config.system.boot.loader.kernelFile}" kexec/bzImage
+      cp "${config.system.build.kexecRun}" kexec/run
+      cp "${pkgs.pkgsStatic.kexec-tools}/bin/kexec" kexec/kexec
+      cp "${iprouteStatic}/bin/ip" kexec/ip
+      ${lib.optionalString (pkgs.stdenv.hostPlatform == pkgs.stdenv.buildPlatform) ''
+        kexec/ip -V
+        kexec/kexec --version
+      ''}
+      tar -czvf $out/${kexecInstallerName}-${pkgs.stdenv.hostPlatform.system}.tar.gz kexec
+    '';
 
-  # Build the kexec installer tarball
-  system.build.kexecInstallerTarball = pkgs.runCommand "kexec-tarball" { } ''
-    mkdir kexec $out
-    cp "${config.system.build.netbootRamdisk}/initrd" kexec/initrd
-    cp "${config.system.build.kernel}/${config.system.boot.loader.kernelFile}" kexec/bzImage
-    cp "${config.system.build.kexecRun}" kexec/run
-    cp "${pkgs.pkgsStatic.kexec-tools}/bin/kexec" kexec/kexec
-    cp "${iprouteStatic}/bin/ip" kexec/ip
-    ${lib.optionalString (pkgs.stdenv.hostPlatform == pkgs.stdenv.buildPlatform) ''
-      kexec/ip -V
-      kexec/kexec --version
-    ''}
-    tar -czvf $out/${kexecInstallerName}-${pkgs.stdenv.hostPlatform.system}.tar.gz kexec
-  '';
-
-  system.stateVersion = "25.05";
+    stateVersion = "25.05";
+  };
 }
