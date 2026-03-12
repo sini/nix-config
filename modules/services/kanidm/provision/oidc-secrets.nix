078c7eef967983c989c255c1f0d743b9b5347a26
Jason Bowman <jason@json64.dev>
2026-03-11 17:25:52 -0700

    feat(longhorn): enable oidc auth

--- modules/services/kanidm/provision/oidc-secrets.nix
+++ modules/services/kanidm/provision/oidc-secrets.nix
@@ -16,10 +16,7 @@
           generator = {
             tags = [ "oidc" ];
             script =
-              {
-                pkgs,
-                ...
-              }:
+              { pkgs, ... }:
               ''
                 # Generate an rfc3986 secret
                 secret=$(${pkgs.openssl}/bin/openssl rand -base64 54 | tr -d '\n' | tr '+/' '-_' | tr -d '=' | cut -c1-72)
@@ -28,7 +25,6 @@
           };
         };
       };
-
     in
     {
       age.secrets = lib.mkMerge (
