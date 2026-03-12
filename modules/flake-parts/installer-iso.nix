4eab0b0947d68d8cbdf0b0cee3b1d0aa7aaab2b7
Jason Bowman <jason@json64.dev>
2026-03-12 12:13:40 -0700

    feat(installer-iso): initial iso generation

---
+++ modules/flake-parts/installer-iso.nix
@@ -0,0 +1,20 @@
+{ inputs, withSystem, ... }:
+{
+  flake.nixosConfigurations.installer-iso = withSystem "x86_64-linux" (
+    { system, ... }:
+    inputs.nixpkgs.lib.nixosSystem {
+      inherit system;
+      modules = [
+        ../hosts/_installer-iso
+      ];
+    }
+  );
+
+  # Add a package output for easy building of the ISO image
+  # Use "iso" to avoid conflict with meta-build-target.nix which creates "installer-iso" package
+  perSystem =
+    { system, ... }:
+    {
+      packages.iso = inputs.self.nixosConfigurations.installer-iso.config.system.build.isoImage;
+    };
+}
