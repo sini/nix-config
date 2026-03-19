2d09a4ecce1520de59301077c2e156d30ca72d4e
Jason Bowman <jason@json64.dev>
2026-03-18 19:17:47 -0700

    feat(darwin): allow @admin users

--- modules/core/nix/nix.nix
+++ modules/core/nix/nix.nix
@@ -1,101 +1,101 @@
 {
   flake.features.nix = {
     system = {
-      nix =
-        let
-          users = [
-            "root"
-            "@wheel"
+      nix = {
+        settings = {
+          experimental-features = [
+            "auto-allocate-uids"
+            "cgroups"
+            "flakes"
+            "nix-command"
+            "pipe-operator"
           ];
-        in
-        {
-          settings = {
-            experimental-features = [
-              "auto-allocate-uids"
-              "cgroups"
-              "flakes"
-              "nix-command"
-              "pipe-operator"
-            ];
 
-            max-jobs = "auto";
+          max-jobs = "auto";
 
-            # Make legacy nix commands use the XDG base directories instead of creating directories in $HOME.
-            use-xdg-base-directories = true;
+          # Make legacy nix commands use the XDG base directories instead of creating directories in $HOME.
+          use-xdg-base-directories = true;
 
-            # The maximum number of parallel TCP connections used to fetch files from binary caches and by other downloads.
-            # It defaults to 25. 0 means no limit.
-            http-connections = 128;
+          # The maximum number of parallel TCP connections used to fetch files from binary caches and by other downloads.
+          # It defaults to 25. 0 means no limit.
+          http-connections = 128;
 
-            # This option defines the maximum number of substitution jobs that Nix will try to run in
-            # parallel. The default is 16. The minimum value one can choose is 1 and lower values will be
-            # interpreted as 1.
-            max-substitution-jobs = 128;
+          # This option defines the maximum number of substitution jobs that Nix will try to run in
+          # parallel. The default is 16. The minimum value one can choose is 1 and lower values will be
+          # interpreted as 1.
+          max-substitution-jobs = 128;
 
-            # The number of lines of the tail of the log to show if a build fails.
-            log-lines = 25;
+          # The number of lines of the tail of the log to show if a build fails.
+          log-lines = 25;
 
-            # When free disk space in /nix/store drops below min-free during a build, Nix performs a
-            # garbage-collection until max-free bytes are available or there is no more garbage.
-            # A value of 0 (the default) disables this feature.
-            min-free = 128000000; # 128 MB
-            max-free = 1000000000; # 1 GB
+          # When free disk space in /nix/store drops below min-free during a build, Nix performs a
+          # garbage-collection until max-free bytes are available or there is no more garbage.
+          # A value of 0 (the default) disables this feature.
+          min-free = 128000000; # 128 MB
+          max-free = 1000000000; # 1 GB
 
-            # Automatically detect files in the store that have identical contents, and replaces
-            # them with hard links to a single copy. This saves disk space.
-            auto-optimise-store = true;
+          # Automatically detect files in the store that have identical contents, and replaces
+          # them with hard links to a single copy. This saves disk space.
+          auto-optimise-store = true;
 
-            # Whether to warn about dirty Git/Mercurial trees.
-            warn-dirty = false;
+          # Whether to warn about dirty Git/Mercurial trees.
+          warn-dirty = false;
 
-            keep-outputs = true;
+          keep-outputs = true;
 
-            keep-derivations = true;
+          keep-derivations = true;
 
-            ################
-            # Substituters #
-            ################
-            substituters = [
-              "https://cache.nixos.org/"
-              "https://nix-community.cachix.org"
-              "https://numtide.cachix.org"
-              "https://attic.xuyh0120.win/lantian" # xddxdd/nix-cachyos-kernel/
-              "https://cache.garnix.io"
-            ];
+          ################
+          # Substituters #
+          ################
+          substituters = [
+            "https://cache.nixos.org/"
+            "https://nix-community.cachix.org"
+            "https://numtide.cachix.org"
+            "https://attic.xuyh0120.win/lantian" # xddxdd/nix-cachyos-kernel/
+            "https://cache.garnix.io"
+          ];
 
-            trusted-public-keys = [
-              "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-              "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-              "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
-              "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc=" # xddxdd/nix-cachyos-kernel
-              "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
-            ];
+          trusted-public-keys = [
+            "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+            "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+            "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
+            "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc=" # xddxdd/nix-cachyos-kernel
+            "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+          ];
 
-            # The timeout (in seconds) for establishing connections in the binary cache substituter.
-            # It corresponds to curl’s –connect-timeout option. A value of 0 means no limit.
-            connect-timeout = 5;
+          # The timeout (in seconds) for establishing connections in the binary cache substituter.
+          # It corresponds to curl’s –connect-timeout option. A value of 0 means no limit.
+          connect-timeout = 5;
 
-            # Allow the use of cachix
-            trusted-users = users;
-            allowed-users = users;
+          builders-use-substitutes = true;
 
-            builders-use-substitutes = true;
+          sandbox = "relaxed";
 
-            sandbox = "relaxed";
-
-            # If set to true, Nix will fall back to building from source if a binary substitute
-            # fails. This is equivalent to the –fallback flag. The default is false.
-            fallback = true;
-          };
-
-          gc = {
-            automatic = true;
-            options = "--delete-older-than 8d";
-          };
+          # If set to true, Nix will fall back to building from source if a binary substitute
+          # fails. This is equivalent to the –fallback flag. The default is false.
+          fallback = true;
         };
+
+        gc = {
+          automatic = true;
+          options = "--delete-older-than 8d";
+        };
+      };
     };
 
     darwin = {
+      nix.settings =
+        let
+          users = [
+            "root"
+            "@admin"
+          ];
+        in
+        {
+          trusted-users = users;
+          allowed-users = users;
+        };
       nix.gc.interval = {
         Hour = 5;
         Minute = 0;
@@ -106,6 +106,18 @@
       { lib, ... }:
       {
         nix = {
+          settings =
+            let
+              users = [
+                "root"
+                "@wheel"
+              ];
+            in
+            {
+              trusted-users = users;
+              allowed-users = users;
+            };
+
           gc.dates = "05:00";
           daemonCPUSchedPolicy = lib.mkDefault "batch";
           daemonIOSchedClass = lib.mkDefault "idle";
