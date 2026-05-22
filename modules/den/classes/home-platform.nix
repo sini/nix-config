38e27900d1444b75cf332a5af9d5ab959119cbe1
Jason Bowman <jason@json64.dev>
2026-05-21 17:48:54 -0700

    feat(den): add homeLinux/homeDarwin/homeAarch64 classes with route policies

---
+++ modules/den/classes/home-platform.nix
@@ -0,0 +1,25 @@
+{ den, lib, ... }:
+{
+  den.classes.homeLinux.description = "Home-manager modules for Linux hosts";
+  den.classes.homeDarwin.description = "Home-manager modules for Darwin hosts";
+  den.classes.homeAarch64.description = "Home-manager modules for aarch64 hosts";
+
+  den.policies.homeLinux-to-hm = { host, ... }:
+    lib.optional (lib.hasSuffix "-linux" host.system)
+      (den.lib.policy.route { fromClass = "homeLinux"; intoClass = "homeManager"; });
+
+  den.policies.homeDarwin-to-hm = { host, ... }:
+    lib.optional (lib.hasSuffix "-darwin" host.system)
+      (den.lib.policy.route { fromClass = "homeDarwin"; intoClass = "homeManager"; });
+
+  den.policies.homeAarch64-to-hm = { host, ... }:
+    lib.optional (lib.hasPrefix "aarch64-" host.system)
+      (den.lib.policy.route { fromClass = "homeAarch64"; intoClass = "homeManager"; });
+
+  # Route policies fire at user scope where host.system is available
+  den.schema.user.includes = [
+    den.policies.homeLinux-to-hm
+    den.policies.homeDarwin-to-hm
+    den.policies.homeAarch64-to-hm
+  ];
+}
