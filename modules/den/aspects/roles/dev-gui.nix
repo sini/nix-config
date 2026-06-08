391c7ccb214831ba264209dc7325377b0f165b0e
Jason Bowman <jason@json64.dev>
2026-06-08 13:29:28 -0700

    chore: enable kube utils for dev-gui role

--- modules/den/aspects/roles/dev-gui.nix
+++ modules/den/aspects/roles/dev-gui.nix
@@ -7,6 +7,14 @@
       apps.dev.git.gitkraken
       apps.dev.networking.wireshark
       apps.dev.zellij
+      apps.dev.k8s.core
+      apps.dev.k8s.dev
+      apps.dev.k8s.helm
+      apps.dev.k8s.observability
+      apps.dev.k8s.plugins
+      apps.dev.k8s.security
+      apps.dev.k8s.tui
+      apps.dev.k8s.utils
     ];
   };
 }
