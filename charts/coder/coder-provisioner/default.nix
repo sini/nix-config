b301742a8375f80533470d4d988ffd427e0e7053
Jason Bowman <jason@json64.dev>
2026-06-28 15:34:26 -0700

    feat(charts): pin coder + coder-provisioner helm charts

---
+++ charts/coder/coder-provisioner/default.nix
@@ -0,0 +1,6 @@
+{
+  repo = "https://helm.coder.com/v2";
+  chart = "coder-provisioner";
+  version = "2.34.4";
+  chartHash = "sha256-N0/nUTfUmcOkJ8Wpbh1MobI8Xh5ZOXTFxmeVi10PDAc=";
+}
---
+++ charts/coder/coder/default.nix
@@ -0,0 +1,6 @@
+{
+  repo = "https://helm.coder.com/v2";
+  chart = "coder";
+  version = "2.34.4";
+  chartHash = "sha256-Op/prXL9hKydMd2nMADwk5Mo4TYhx6XQ8/qC69CcvVw=";
+}
