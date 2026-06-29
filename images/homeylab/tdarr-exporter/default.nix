453faa8436090e7c285a1d7579ee2be336aa1220
Jason Bowman <jason@json64.dev>
2026-06-29 11:40:12 -0700

    feat(media): pin tdarr server + exporter images

---
+++ images/homeylab/tdarr-exporter/default.nix
@@ -0,0 +1,9 @@
+{
+  imageName = "docker.io/homeylab/tdarr-exporter";
+  imageTag = "3.0.0";
+  imageDigest = "sha256:07bfc1b72ecec73a343be2007c22373b6e15acbdbecdd29545ab5a03ca21ca69";
+  imageHash = "sha256-XDrey2PhSTbYNto9oM81VPJ5oGy32LyJYkK2XioAtHY=";
+  arch = "amd64";
+  os = "linux";
+  pinned = true;
+}
