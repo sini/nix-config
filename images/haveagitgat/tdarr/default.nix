5d38388854d444ffcc3d1a734da4ff257308358b
Jason Bowman <jason@json64.dev>
2026-06-29 11:40:12 -0700

    feat(media): pin tdarr server + exporter images

---
+++ images/haveagitgat/tdarr/default.nix
@@ -0,0 +1,9 @@
+{
+  imageName = "ghcr.io/haveagitgat/tdarr";
+  imageTag = "latest";
+  imageDigest = "sha256:71360670ac0c39e1bb8daf2b0c5c9ebb92b5d41e79c54128f2cea5bbdc32ddf2";
+  imageHash = "sha256-qQo5HhWrxq+TyVamN5m0lKEOLLrVf0wVzmlCbWo08VM=";
+  arch = "amd64";
+  os = "linux";
+  pinned = false;
+}
