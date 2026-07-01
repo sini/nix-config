bfb5e5b5603bbca773ff5422c406ad941550420c
Jason Bowman <jason@json64.dev>
2026-07-01 13:08:35 -0700

    feat(openssh): modern transport crypto (kex/ciphers/macs)

--- modules/den/aspects/core/security/openssh.nix
+++ modules/den/aspects/core/security/openssh.nix
@@ -11,6 +11,22 @@
           PermitRootLogin = "prohibit-password";
           PasswordAuthentication = false;
           KbdInteractiveAuthentication = false;
+
+          KexAlgorithms = [
+            "curve25519-sha256"
+            "curve25519-sha256@libssh.org"
+            "sntrup761x25519-sha512@openssh.com"
+          ];
+          Ciphers = [
+            "chacha20-poly1305@openssh.com"
+            "aes256-gcm@openssh.com"
+            "aes128-gcm@openssh.com"
+          ];
+          Macs = [
+            "hmac-sha2-512-etm@openssh.com"
+            "hmac-sha2-256-etm@openssh.com"
+            "umac-128-etm@openssh.com"
+          ];
         };
 
         extraConfig = ''
@@ -35,6 +51,9 @@
           AllowAgentForwarding yes
           AllowStreamLocalForwarding yes
           AuthenticationMethods publickey
+          KexAlgorithms curve25519-sha256,curve25519-sha256@libssh.org,sntrup761x25519-sha512@openssh.com
+          Ciphers chacha20-poly1305@openssh.com,aes256-gcm@openssh.com,aes128-gcm@openssh.com
+          MACs hmac-sha2-512-etm@openssh.com,hmac-sha2-256-etm@openssh.com,umac-128-etm@openssh.com
         '';
       };
     };
