25196b9dfc52b3fa31124e69ec0c38e7a8412a04
Jason Bowman <jason@json64.dev>
2026-07-06 10:23:17 -0700

    feat(dev-gui): add antigravity editor to the dev-gui role

--- modules/den/aspects/roles/dev-gui.nix
+++ modules/den/aspects/roles/dev-gui.nix
@@ -3,6 +3,7 @@
   den.aspects.roles.dev-gui = {
     includes = with den.aspects; [
       apps.dev.security.gpg
+      apps.dev.editor.antigravity
       apps.dev.editor.vscode
       apps.dev.git.gitkraken
       apps.dev.networking.wireshark
