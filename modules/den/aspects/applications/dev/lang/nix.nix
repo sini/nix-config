8f634a5b327d77eef7484aaafaa4632e9bc28013
Jason Bowman <jason@json64.dev>
2026-09-23 11:36:59 -0700

    feat(nix): swap nixd for nil in vscode

--- modules/den/aspects/applications/dev/lang/nix.nix
+++ modules/den/aspects/applications/dev/lang/nix.nix
@@ -21,9 +21,9 @@
       [
         {
           "nix.enableLanguageServer" = true;
-          "nix.serverPath" = lib.getExe pkgs.nil;
+          "nix.serverPath" = lib.getExe pkgs.nixd;
           "nix.serverSettings" = {
-            "nil" = {
+            nil = {
               "nix" = {
                 "flake" = {
                   "autoArchive" = true;
@@ -35,13 +35,36 @@
                 "command" = [ (lib.getExe pkgs.nixfmt) ];
               };
             };
+            nixd = {
+              # nixpkgs.expr = "import (builtins.getFlake (toString ./.)).inputs.nixpkgs { }";
+              formatting.command = [ (lib.getExe pkgs.nixfmt) ];
+              # options.nixos.expr = "(builtins.getFlake (toString ./.)).nixosConfigurations.cortex.options";
+              # options.home-manager.expr = "(builtins.getFlake (builtins.toString ./.)).nixosConfigurations.cortex.options.home-manager.users.type.getSubOptions []";
+            };
           };
+
+          "nix.hiddenLanguageServerErrors" = [ "textDocument/definition" ];
 
           "[nix]" = {
             "editor.defaultFormatter" = "jnoortheen.nix-ide";
             "editor.formatOnSave" = true;
             "editor.formatOnPaste" = true;
             "editor.tabSize" = 2;
+            "path-autocomplete.triggerOutsideStrings" = true;
+            "path-autocomplete.enableFolderTrailingSlash" = false;
+            "editor.fontLigatures" = lib.concatMapStringsSep ", " (s: "'${s}'") [
+              "ss01" # == === =/= != !== /= /== ~~ =~ !~
+              "ss02" # >= <=
+              # "ss03" # -> <- => <!-- --> <~ <~~ <~>
+              # "ss04" # </ /> </> /\ \/
+              # "ss05" # |> <|
+              "ss06" # ## ###
+              "ss07" # *** /* */ /*/ (* *) (*)
+              # "ss08" # .= .- ..<
+              "liga" # <! !! ** :: =: == =! =/ != --
+              # "calt" # // /// && ?? ?. ?: || :: ::: ;; .. ... =~= #= := =:= :> >: :> ..= ==-
+              # "dlig" # all
+            ];
           };
         }
       ];
